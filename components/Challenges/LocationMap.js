68fd15a78405bfb083a14f1f635fa6672e72dfc1
Amanda Bullington <albullington@gmail.com>
2018-10-15 16:50:50 -0700

    Moved state into LocationPicker to pass props back to MainScreen

--- components/Challenges/LocationMap.js
+++ components/Challenges/LocationMap.js
@@ -1,49 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import { Image, View } from "react-native";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 import styles from "../../styles/locationPicker";
 import marker from "../../assets/marker.png";
 
-const latitudeDelta = 0.025;
-const longitudeDelta = 0.025;
 
-class LocationMap extends Component {
-  constructor( { latitude, longitude } ) {
-    super();
-
-    this.state = {
-      region: {
-        latitudeDelta,
-        longitudeDelta,
-        latitude,
-        longitude
-      }
-    };
-  }
-
-  onRegionChange( region ) {
-    this.setState( {
-      region
-    } );
-  }
-
-  render() {
-    const { region } = this.state;
-
-    return (
-      <View style={{ flex: 1 }}>
-        <MapView
-          provider={PROVIDER_DEFAULT}
-          style={styles.map}
-          region={region}
-        />
-        <View pointerEvents="none" style={styles.markerFixed}>
-          <Image style={styles.marker} source={marker} />
-        </View>
-      </View>
-    );
-  }
-}
+const LocationMap = ( { region, onRegionChange } ) => (
+  <View style={{ flex: 1 }}>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      style={styles.map}
+      region={region}
+    />
+    <View pointerEvents="none" style={styles.markerFixed}>
+      <Image style={styles.marker} source={marker} />
+    </View>
+  </View>
+);
 
 export default LocationMap;
--- components/Challenges/LocationPicker.js
+++ components/Challenges/LocationPicker.js
@@ -1,32 +1,66 @@
 // @flow
 
-import React from "react";
+import React, { Component } from "react";
 import { TouchableHighlight, Text, View } from "react-native";
 import LocationMap from "./LocationMap";
 
 import styles from "../../styles/locationPicker";
 
+const latitudeDelta = 0.025;
+const longitudeDelta = 0.025;
+
 type Props = {
   navigation: any
 }
 
-const LocationPicker = ( { navigation }: Props ) => {
-  const { location, latitude, longitude } = navigation.state.params;
+class LocationPicker extends Component {
+  constructor( { navigation }: Props ) {
+    super();
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.headerText}>
-        Looking for species in a 50 mile radius around this point:
-      </Text>
-      <Text style={styles.locationText}>{location}</Text>
-      <View style={styles.mapContainer}>
-        <LocationMap latitude={latitude} longitude={longitude} />
+    const { location, latitude, longitude } = navigation.state.params;
+
+    this.state = {
+      region: {
+        latitudeDelta,
+        longitudeDelta,
+        latitude,
+        longitude
+      },
+      location
+    };
+
+    this.onRegionChange = this.onRegionChange.bind( this );
+  }
+
+  onRegionChange( region ) {
+    this.setState( {
+      region
+    } );
+    console.log( "region changed to: ", region );
+  }
+
+  render() {
+    const { region, location } = this.state;
+    const { navigation } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headerText}>
+          Looking for species in a 50 mile radius around this point:
+        </Text>
+        <Text style={styles.locationText}>{location}</Text>
+        <View style={styles.mapContainer}>
+          <LocationMap
+            region={region}
+            onRegionChange={this.onRegionChange}
+          />
+        </View>
+        <TouchableHighlight style={styles.button}>
+          <Text style={styles.buttonText} onPress={() => navigation.navigate( "Main", { latitude: region.latitude, longitude: region.longitude } )}>Done</Text>
+        </TouchableHighlight>
       </View>
-      <TouchableHighlight style={styles.button}>
-        <Text style={styles.buttonText} onPress={() => navigation.navigate( "Main" )}>Done</Text>
-      </TouchableHighlight>
-    </View>
-  );
-};
+    );
+  }
+}
 
 export default LocationPicker;

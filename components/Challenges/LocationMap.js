6919ba31ef47d0271e08456a9dc080a5df6a47a4
Amanda Bullington <albullington@gmail.com>
2018-10-15 17:40:43 -0700

    Typechecking for LocationMap component

--- components/Challenges/LocationMap.js
+++ components/Challenges/LocationMap.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from "react";
 import { Image, View } from "react-native";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
@@ -5,13 +7,19 @@ import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 import styles from "../../styles/locationPicker";
 import marker from "../../assets/marker.png";
 
+type Props = {
+  region: Object,
+  onRegionChange: Function
+}
 
-const LocationMap = ( { region, onRegionChange } ) => (
+
+const LocationMap = ( { region, onRegionChange }: Props ) => (
   <View style={{ flex: 1 }}>
     <MapView
       provider={PROVIDER_DEFAULT}
       style={styles.map}
       region={region}
+      onRegionChangeComplete={region => onRegionChange( region )}
     />
     <View pointerEvents="none" style={styles.markerFixed}>
       <Image style={styles.marker} source={marker} />

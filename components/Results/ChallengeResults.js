6b7dbe1a5682224493aed24a5d7928fe241acb39
Amanda Bullington <albullington@gmail.com>
2018-10-17 17:15:51 -0700

    Created text and button for results screen

--- components/Results/ChallengeResults.js
+++ components/Results/ChallengeResults.js
@@ -1,15 +1,56 @@
-import React from "react";
-import { View, ImageBackground } from "react-native";
+import React, { Component } from "react";
+import {
+  View,
+  ImageBackground,
+  Text,
+  TouchableHighlight
+} from "react-native";
 
 import styles from "../../styles/results";
 
-const ChallengeResults = () => (
-  <View>
-    <ImageBackground
-      style={styles.backgroundImage}
-      source={require( "../../assets/backgrounds/background.png" )}
-    />
-  </View>
-);
+class ChallengeResults extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      headerText: "It's a match",
+      text: "You saw a California Poppy",
+      buttonText: "Add to collection",
+      taxon: null,
+      match: false,
+      photoClear: false
+    };
+  }
+
+  render() {
+    const { headerText, text, buttonText } = this.state;
+
+    return (
+      <View style={ { flex: 1 } }>
+        <View style={styles.container}>
+          <ImageBackground
+            style={styles.backgroundImage}
+            source={require( "../../assets/backgrounds/background.png" )}
+          >
+            <View style={styles.header}>
+              <Text style={styles.headerText}>{headerText}</Text>
+              <Text style={styles.text}>{text}</Text>
+            </View>
+            <View>
+              <TouchableHighlight style={styles.button}>
+                <Text
+                  style={styles.buttonText}
+                  onPress={() => console.log( "pressed button" )}
+                >
+                  {buttonText}
+                </Text>
+              </TouchableHighlight>
+            </View>
+          </ImageBackground>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default ChallengeResults;
--- styles/results.js
+++ styles/results.js
@@ -5,6 +5,44 @@ const { width, height } = Dimensions.get( "screen" );
 export default StyleSheet.create( {
   backgroundImage: {
     width,
-    height
+    height,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  container: {
+    flex: 1
+  },
+  header: {
+    width: 100
+  },
+  headerText: {
+    fontSize: 20,
+    lineHeight: 18,
+    color: "#F5FCFF",
+    fontFamily: "Whitney-Medium"
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 14,
+    color: "#F5FCFF",
+    fontFamily: "Whitney-Medium",
+    flexWrap: "wrap"
+  },
+  button: {
+    backgroundColor: "green",
+    justifyContent: "flex-end",
+    marginHorizontal: 40,
+    marginBottom: 10,
+    marginTop: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius: 40
+  },
+  buttonText: {
+    fontFamily: "Whitney-Semibold",
+    fontSize: 18,
+    color: "white",
+    textAlign: "center",
+    justifyContent: "center"
   }
 } );

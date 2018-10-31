a6303f5a69f058aaf7d83859d2fd76ee92a8983f
Amanda Bullington <albullington@gmail.com>
2018-10-31 16:42:38 -0700

    Users can click from SpeciesDetail page into the camera flow

--- components/Species/SpeciesDetail.js
+++ components/Species/SpeciesDetail.js
@@ -296,7 +296,10 @@ class SpeciesDetail extends Component {
           </ScrollView>
         </View>
         <View style={styles.footer}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate( "CameraCapture" )}
+          >
             <Text style={styles.buttonText}>Found it!</Text>
           </TouchableOpacity>
         </View>

e7e15924be3febd9ab3fc9068e645b8043ec12e5
Amanda Bullington <albullington@gmail.com>
2018-11-19 15:58:40 -0800

    Created a marker on species map when species was already collected

--- components/Species/SpeciesMap.js
+++ components/Species/SpeciesMap.js
@@ -1,7 +1,7 @@
 // @flow
 import React from "react";
 import { View } from "react-native";
-import MapView, { PROVIDER_DEFAULT, UrlTile } from "react-native-maps";
+import MapView, { PROVIDER_DEFAULT, UrlTile, Marker } from "react-native-maps";
 
 import styles from "../../styles/species";
 
@@ -10,7 +10,10 @@ type Props = {
   id: number
 }
 
-const LocationMap = ( { region, id }: Props ) => (
+const LocationMap = ( {
+  region,
+  id
+}: Props ) => (
   <View style={styles.mapContainer}>
     <MapView
       region={region}
@@ -23,6 +26,10 @@ const LocationMap = ( { region, id }: Props ) => (
     >
       <UrlTile
         urlTemplate={`https://api.inaturalist.org/v1/colored_heatmap/{z}/{x}/{y}.png?taxon_id=${id}&color=darkorange`}
+      />
+      <Marker
+        coordinate={{ latitude: region.latitude, longitude: region.longitude }}
+        pinColor="tomato"
       />
     </MapView>
   </View>

3547f9dd117a14b2b4b57eee00485092fc7336fc
Amanda Bullington <albullington@gmail.com>
2018-12-27 14:23:28 -0800

    Moved challenge screen into main screen

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -8,12 +8,19 @@ import {
   PermissionsAndroid,
   Platform,
   View,
-  SafeAreaView
+  SafeAreaView,
+  ImageBackground
 } from "react-native";
 import { NavigationEvents } from "react-navigation";
 
 import realmConfig from "../models/index";
-import ChallengeScreen from "./Challenges/ChallengeScreen";
+
+import Banner from "./Banner";
+import ChallengeGrid from "./Challenges/ChallengeGrid";
+import ChallengeHeader from "./Challenges/ChallengeHeader";
+import ChallengeFooter from "./Challenges/ChallengeFooter";
+import LoadingWheel from "./LoadingWheel";
+import ErrorScreen from "./ErrorScreen";
 import styles from "../styles/challenges";
 import taxonIds from "../utility/taxonDict";
 import {
@@ -212,7 +219,6 @@ class MainScreen extends Component<Props, State> {
       latitude,
       longitude,
       location,
-      profileIcon,
       badgeCount,
       speciesCount,
       taxaType,
@@ -224,6 +230,26 @@ class MainScreen extends Component<Props, State> {
       navigation
     } = this.props;
 
+    let challenges;
+
+    if ( error ) {
+      challenges = <ErrorScreen error={error} />;
+    } else if ( loading ) {
+      challenges = <LoadingWheel />;
+    } else if ( taxa.length === 0 ) {
+      challenges = <ErrorScreen error={`We couldn't find any ${taxaType} in this location. Please try again.`} />;
+    } else {
+      challenges = (
+        <ChallengeGrid
+          navigation={navigation}
+          taxa={taxa}
+          latitude={latitude}
+          longitude={longitude}
+          location={location}
+        />
+      );
+    }
+
     return (
       <SafeAreaView style={styles.safeContainer}>
         <View style={styles.mainContainer}>
@@ -233,21 +259,37 @@ class MainScreen extends Component<Props, State> {
               this.fetchUserLocation();
             }}
           />
-          <ChallengeScreen
-            taxa={taxa}
-            taxaType={taxaType}
-            latitude={latitude}
-            loading={loading}
-            longitude={longitude}
-            location={location}
-            profileIcon={profileIcon}
-            navigation={navigation}
-            badgeCount={badgeCount}
-            speciesCount={speciesCount}
-            taxaName={taxaName}
-            error={error}
-            id={id}
-          />
+          <ImageBackground
+            style={styles.backgroundImage}
+            source={require( "../assets/backgrounds/background.png" )}
+          >
+            <View style={styles.container}>
+              { taxaName ? (
+                <Banner
+                  bannerText={`${taxaName} collected!`}
+                  main
+                  id={id}
+                />
+              ) : null }
+              <ChallengeHeader
+                latitude={latitude}
+                longitude={longitude}
+                location={location}
+                loading={loading}
+                navigation={navigation}
+                taxaType={taxaType}
+                taxaName={taxaName}
+              />
+              {challenges}
+              <ChallengeFooter
+                latitude={latitude}
+                longitude={longitude}
+                navigation={navigation}
+                badgeCount={badgeCount}
+                speciesCount={speciesCount}
+              />
+            </View>
+          </ImageBackground>
         </View>
       </SafeAreaView>
     );

1e92d971c47d7a362acf287ee83641f8e3f5553c
Amanda Bullington <albullington@gmail.com>
2019-01-02 14:41:15 -0800

    Passing location, lat, and lng to species detail screen

--- components/Home/SpeciesNearby.js
+++ components/Home/SpeciesNearby.js
@@ -54,9 +54,9 @@ const SpeciesNearby = ( {
             <TouchableOpacity
               onPress={ () => navigation.push( "Species", {
                 id: item.id,
-                latitude: null,
-                longitude: null,
-                location: null,
+                latitude,
+                longitude,
+                location,
                 seen: false,
                 commonName: capitalizeNames( item.preferred_common_name ),
                 scientificName: item.name

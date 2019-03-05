73719d98b8e58bafe7b1a7fe1242e325daea38b2
Alex Shepard <alex@inaturalist.org>
2019-03-05 14:20:23 -0800

    add code to migrate the user's realm database

--- ios/Seek/AppDelegate.m
+++ ios/Seek/AppDelegate.m
@@ -10,10 +10,17 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+static NSString *hasMigratedRealmDatabaseFromContainer = @"HasMigratedRealmDatabaseFromContainer";
+static NSString *appGroupId = @"group.org.inaturalist.CardsSharing";
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if (![[NSUserDefaults standardUserDefaults] boolForKey:hasMigratedRealmDatabaseFromContainer]) {
+    [self migrateRealmDatabaseFromSharedContainer];
+  }
+  
   NSURL *jsCodeLocation;
 
   #ifdef DEBUG
@@ -34,6 +41,39 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (void)migrateRealmDatabaseFromSharedContainer {
+  NSString *realmDbFilename = @"db.realm";
+  // fm lets us do operations on the filesystem
+  NSFileManager *fm = [NSFileManager defaultManager];
+  // base url for the shared container
+  NSURL *containerUrl = [fm containerURLForSecurityApplicationGroupIdentifier:appGroupId];
+  if (containerUrl) {
+    // url for the realm database in the container
+    NSURL *containerRealmUrl = [containerUrl URLByAppendingPathComponent:realmDbFilename];
+    if ([fm fileExistsAtPath:containerRealmUrl.path]) {
+      // get the app document directory
+      NSURL *documentsUrl =   [[fm URLsForDirectory:NSDocumentDirectory
+                                          inDomains:NSUserDomainMask] lastObject];
+      // url for the realm database in the app documents directory
+      NSURL *documentsRealmUrl = [documentsUrl URLByAppendingPathComponent:realmDbFilename];
+      // move error will go here
+      NSError *moveError = nil;
+      // perform the move
+      [fm moveItemAtURL:containerRealmUrl toURL:documentsRealmUrl error:&moveError];
+      if (moveError) {
+        NSLog(@"error moving file: %@", moveError.localizedDescription);
+      }
+    }
+  }
+
+  
+  
+  // update the user defaults
+  [[NSUserDefaults standardUserDefaults] setBool:YES
+                                          forKey:hasMigratedRealmDatabaseFromContainer];
+  [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 @end

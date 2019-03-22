4345eef16eb3ceb2b91b2c57d5a944a75199f2c8
Amanda Bullington <albullington@gmail.com>
2019-03-21 17:00:30 -0700

    Updated challenge completed notification

--- assets/notifications.js
+++ assets/notifications.js
@@ -1,7 +1,7 @@
 const notifications = {
   bird: require( "./logos/logo-inaturalist-bird.png" ),
   badge_empty: require( "./badges/Badge_Empty.png" ),
-  op: require( "./logos/logo-OPwhite.png" )
+  op: require( "./logos/logo-OPblack.png" )
 };
 
 export default notifications;

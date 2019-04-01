0aab8d019e02fb26b3933eec94e7301cbecbb9e9
Amanda Bullington <albullington@gmail.com>
2019-04-01 12:58:06 -0700

    Updated version number and commented out commonNames

--- components/Menu/AboutScreen.js
+++ components/Menu/AboutScreen.js
@@ -57,7 +57,7 @@ const AboutScreen = ( { navigation }: Props ) => (
         <View style={styles.block}>
           <Text style={styles.greenText}>
             {i18n.t( "about.version" ).toLocaleUpperCase()}
-            {" 2.0.0 (16)"}
+            {" 2.0.0 (17)"}
           </Text>
         </View>
         <Padding />

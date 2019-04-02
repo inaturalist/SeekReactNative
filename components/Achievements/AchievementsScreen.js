fee61717ec11d0b200e1df8ab66c2a7e65f6ca20
Amanda Bullington <albullington@gmail.com>
2019-04-02 14:16:34 -0700

    Recalculating badges on achievements screen

--- components/Achievements/AchievementsScreen.js
+++ components/Achievements/AchievementsScreen.js
@@ -30,6 +30,7 @@ import LevelModal from "../AchievementModals/LevelModal";
 import ChallengeModal from "../AchievementModals/ChallengeModal";
 import ChallengeUnearnedModal from "../AchievementModals/ChallengeUnearnedModal";
 import GreenHeader from "../GreenHeader";
+import { recalculateBadges } from "../../utility/badgeHelpers";
 
 type Props = {
   navigation: any
@@ -60,6 +61,8 @@ class AchievementsScreen extends Component<Props> {
   }
 
   fetchBadges() {
+    recalculateBadges();
+
     Realm.open( realmConfig )
       .then( ( realm ) => {
         const badges = realm.objects( "BadgeRealm" );

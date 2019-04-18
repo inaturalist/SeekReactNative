f706434f609d4a8f32630115f31df918681230a6
Amanda Bullington <albullington@gmail.com>
2019-04-18 16:33:37 -0700

    Updated challenges / missions dict to include june - nov challenges

--- utility/missionsDict.js
+++ utility/missionsDict.js
@@ -26,6 +26,54 @@ const missions = {
       number: 3,
       types: ["fungi"]
     }
+  },
+  2: {
+    0: {
+      number: 6,
+      types: ["plants"]
+    },
+    1: {
+      number: 3,
+      types: ["Orthoptera", "Lepidoptera", "Bovines", "Camelids", "Giraffes", "Proboscidea", "Diprotodontia", "Equines", "Lagomorpha", "Rodents", "Sirenians"]
+    },
+    3: {
+      number: 3,
+      types: ["carnivora", "spiders", "mantid"]
+    },
+    4: {
+      number: 2,
+      types: ["fungi"]
+    }
+  },
+  3: {
+    0: {
+      number: 10,
+      types: ["Pinales", "Arecaceae", "Fagales", "Sapindales", "Magnoliales", "Aquifoliales", "Sorbus", "Prunus", "Pyrus", "Malus", "Celtis", "Populus", "Salix", "Laurales", "Ulmaceae", "Rhamnaceae", "Moraceae", "Rhizophoraceae", "Combretaceae", "Cornales"]
+    }
+  },
+  4: {
+    0: {
+      number: 10,
+      types: ["insects"]
+    }
+  },
+  5: {
+    0: {
+      number: 10,
+      types: ["Odonata", "Amphibia", "Actinopterygii", "Anseriformes", "Nymphaeales", "Pontederiaceae", "Lemnoideae", "Typhaceae", "Polypodiopsida", "Bryophyta"]
+    }
+  },
+  6: {
+    0: {
+      number: 5,
+      types: ["birds"]
+    }
+  },
+  7: {
+    0: {
+      number: 20,
+      types: ["plants"]
+    }
   }
 };
 

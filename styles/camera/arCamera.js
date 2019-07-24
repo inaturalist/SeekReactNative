8f824ec98cbef19a1a8b6a57b8543d5b871abf99
Amanda Bullington <albullington@gmail.com>
2019-07-23 17:19:51 -0700

    Styling tweak for Android camera preview

--- styles/camera/arCamera.js
+++ styles/camera/arCamera.js
@@ -10,7 +10,7 @@ export default StyleSheet.create( {
     backgroundColor: colors.black
   },
   camera: {
-    width,
+    width: Platform.OS === "android" ? width + 100 : width, // this should account for offcenter photos on android
     height,
     zIndex: -2
   },

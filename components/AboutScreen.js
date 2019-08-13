ed8c8c9cc0fc14ff167684fd36b73b3207b5d52e
Amanda Bullington <albullington@gmail.com>
2019-08-13 16:12:56 -0700

    Updated to v2.3.1 and build 55 for beta

--- components/AboutScreen.js
+++ components/AboutScreen.js
@@ -30,8 +30,8 @@ class AboutScreen extends Component<Props> {
 
   render() {
     const { navigation } = this.props;
-    const version = "2.3.0";
-    const buildNumber = 54;
+    const version = "2.3.1";
+    const buildNumber = 55;
 
     return (
       <View style={styles.container}>

004c88d7cdbfd5c701bd7d4dec09d1736a46efda
Amanda Bullington <albullington@gmail.com>
2019-08-13 16:56:10 -0700

    Updated build number to 56

--- components/AboutScreen.js
+++ components/AboutScreen.js
@@ -31,7 +31,7 @@ class AboutScreen extends Component<Props> {
   render() {
     const { navigation } = this.props;
     const version = "2.3.1";
-    const buildNumber = 55;
+    const buildNumber = 56;
 
     return (
       <View style={styles.container}>

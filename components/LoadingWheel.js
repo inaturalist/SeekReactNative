29818974c3aaf1e1c3a699e736196951f3256abd
Amanda Bullington <albullington@gmail.com>
2019-09-27 16:48:25 -0700

    Minor tweak

--- components/LoadingWheel.js
+++ components/LoadingWheel.js
@@ -1,16 +1,12 @@
 // @flow
 
 import React from "react";
-import {
-  ActivityIndicator
-} from "react-native";
+import { ActivityIndicator } from "react-native";
 
 type Props = {
   color: string
 }
 
-const LoadingWheel = ( { color }: Props ) => (
-  <ActivityIndicator color={color} size="large" />
-);
+const LoadingWheel = ( { color }: Props ) => <ActivityIndicator color={color} size="large" />;
 
 export default LoadingWheel;

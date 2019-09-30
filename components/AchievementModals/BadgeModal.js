300a3e9d194db50c561ef7c0478c11ec08888436
Amanda Bullington <albullington@gmail.com>
2019-09-30 15:25:18 -0700

    Code cleanup in badge modal

--- components/AchievementModals/BadgeModal.js
+++ components/AchievementModals/BadgeModal.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from "react";
+import React from "react";
 import {
   View,
   Text,
@@ -24,114 +24,75 @@ type Props = {
   toggleBadgeModal: Function
 };
 
-class BadgeModal extends Component<Props> {
-  constructor() {
-    super();
+const BadgeModal = ( { badges, iconicSpeciesCount, toggleBadgeModal }: Props ) => {
+  const badgeList = [];
 
-    this.state = {
-      interval: 0
-    };
-  }
-
-  // scrollToB = () => {
-  //   this.setState( {
-  //     interval: width * 1
-  //   } );
-  // }
-
-  // scrollToC = () => {
-  //   this.setState( {
-  //     interval: width * 2
-  //   } );
-  // }
-
-  // scrollToTop = () => {
-  //   this.setState( {
-  //     interval: 0
-  //   } );
-  // }
-
-  render() {
-    const { badges, iconicSpeciesCount, toggleBadgeModal } = this.props;
-    // const { interval } = this.state;
-
-    const badgeList = [];
-
-    badges.forEach( ( badge, i ) => {
-      const badgeInfo = (
-        <View
-          key={`badge${badge}${i}`}
-          style={styles.carousel}
-        >
-          {badge.earned ? (
-            <Image source={badgeImages[badge.earnedIconName]} style={styles.image} />
-          ) : (
-            <ImageBackground
-              source={badgeImages[badge.unearnedIconName]}
-              style={styles.image}
-              imageStyle={styles.imageStyle}
-            >
-              <LargeProgressCircle badge={badge} iconicSpeciesCount={iconicSpeciesCount} />
-            </ImageBackground>
-          )}
-          {badge.earned
-            ? <Text style={styles.headerText}>{i18n.t( badge.intlName ).toLocaleUpperCase()}</Text>
-            : <Text style={styles.headerText}>{i18n.t( "badges.to_earn" ).toLocaleUpperCase()}</Text>
-          }
-          <Text style={styles.nameText}>
-            {i18n.t( "badges.observe_species" )}
-            {" "}
-            {i18n.t( badge.infoText )}
-          </Text>
-        </View>
-      );
-      badgeList.push( badgeInfo );
-    } );
-
-    return (
-      <SafeAreaView style={styles.safeView}>
-        <View style={styles.outerContainer}>
-          <View style={styles.innerContainer}>
-            <BannerHeader
-              text={i18n.t( badges[0].iconicTaxonName ).toLocaleUpperCase()}
-              modal
-            />
-            <FlatList
-              horizontal
-              pagingEnabled
-              showsHorizontalScrollIndicator={false}
-              data={badgeList}
-              renderItem={( { item } ) => item}
-            />
-            <Image source={icons.badgeSwipeRight} style={styles.arrow} />
-            <View style={styles.row}>
-              <TouchableOpacity onPress={this.scrollToB}>
-                <Image
-                  source={badges[0].earned ? badgeImages[badges[0].earnedIconName] : badgeImages[badges[0].unearnedIconName]}
-                  style={styles.smallImage}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={this.scrollToC}>
-                <Image
-                  source={badges[1].earned ? badgeImages[badges[1].earnedIconName] : badgeImages[badges[1].unearnedIconName]}
-                  style={styles.smallImage}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={this.scrollToTop}>
-                <Image
-                  source={badges[2].earned ? badgeImages[badges[2].earnedIconName] : badgeImages[badges[2].unearnedIconName]}
-                  style={styles.smallImage}
-                />
-              </TouchableOpacity>
-            </View>
-          </View>
-          <TouchableOpacity style={styles.backButton} onPress={() => toggleBadgeModal()}>
-            <Image source={icons.closeModal} />
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
+  badges.forEach( ( badge, i ) => {
+    const badgeInfo = (
+      <View
+        key={`badge${badge}${i}`}
+        style={styles.carousel}
+      >
+        {badge.earned ? (
+          <Image source={badgeImages[badge.earnedIconName]} style={styles.image} />
+        ) : (
+          <ImageBackground
+            source={badgeImages[badge.unearnedIconName]}
+            style={styles.image}
+            imageStyle={styles.imageStyle}
+          >
+            <LargeProgressCircle badge={badge} iconicSpeciesCount={iconicSpeciesCount} />
+          </ImageBackground>
+        )}
+        {badge.earned
+          ? <Text style={styles.headerText}>{i18n.t( badge.intlName ).toLocaleUpperCase()}</Text>
+          : <Text style={styles.headerText}>{i18n.t( "badges.to_earn" ).toLocaleUpperCase()}</Text>
+        }
+        <Text style={styles.nameText}>
+          {i18n.t( "badges.observe_species" )}
+          {" "}
+          {i18n.t( badge.infoText )}
+        </Text>
+      </View>
     );
-  }
-}
+    badgeList.push( badgeInfo );
+  } );
+
+  return (
+    <SafeAreaView style={styles.safeView}>
+      <View style={styles.outerContainer}>
+        <View style={styles.innerContainer}>
+          <BannerHeader
+            text={i18n.t( badges[0].iconicTaxonName ).toLocaleUpperCase()}
+            modal
+          />
+          <FlatList
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            data={badgeList}
+            renderItem={( { item } ) => item}
+          />
+          <Image source={icons.badgeSwipeRight} style={styles.arrow} />
+          <View style={styles.row}>
+            {[0, 1, 2].map( item => (
+              <View key={item}>
+                <Image
+                  source={badges[item].earned
+                    ? badgeImages[badges[item].earnedIconName]
+                    : badgeImages[badges[item].unearnedIconName]}
+                  style={styles.smallImage}
+                />
+              </View>
+            ) )}
+          </View>
+        </View>
+        <TouchableOpacity style={styles.backButton} onPress={() => toggleBadgeModal()}>
+          <Image source={icons.closeModal} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default BadgeModal;

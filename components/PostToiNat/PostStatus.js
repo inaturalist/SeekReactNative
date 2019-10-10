ae15a1da2e06da2aa8ddcb7cf57257a37209f9cb
Amanda Bullington <albullington@gmail.com>
2019-10-10 11:13:46 -0700

    Fixed styling error in posting status screen

--- components/PostToiNat/PostStatus.js
+++ components/PostToiNat/PostStatus.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
   View,
-  TouchableOpacity,
   Text,
-  Image
+  Image,
+  StatusBar
 } from "react-native";
 
 import i18n from "../../i18n";
@@ -11,6 +11,9 @@ import styles from "../../styles/posting/postStatus";
 import LoadingWheel from "../UIComponents/LoadingWheel";
 import { colors } from "../../styles/global";
 import posting from "../../assets/posting";
+import GreenButton from "../UIComponents/GreenButton";
+import SafeAreaView from "../UIComponents/SafeAreaView";
+import GreenText from "../UIComponents/GreenText";
 
 type Props = {
   +loading: boolean,
@@ -32,50 +35,50 @@ const PostStatus = ( {
   if ( loading ) {
     headerText = i18n.t( "posting.posting" ).toLocaleUpperCase();
     image = <LoadingWheel color={colors.seekiNatGreen} />;
-    extraText = <Text style={styles.text}>{i18n.t( "posting.wait" )}</Text>;
+    extraText = i18n.t( "posting.wait" );
   } else if ( !loading && postingSuccess ) {
     headerText = i18n.t( "posting.posting_success" ).toLocaleUpperCase();
     image = <Image source={posting.iNatuploading} />;
   } else {
     headerText = i18n.t( "posting.posting_failure" ).toLocaleUpperCase();
     image = <Image source={posting.uploadfail} />;
-    extraText = <Text style={styles.text}>{i18n.t( "posting.internet" )}</Text>;
+    extraText = i18n.t( "posting.internet" );
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={[
-        styles.header,
-        ( !loading && !postingSuccess ) && { color: colors.seekTeal }
-      ]}
-      >
-        {headerText}
-      </Text>
-      <View style={{ marginTop: 54 }} />
-      {image}
-      {extraText}
+    <>
+      <SafeAreaView color={colors.white} />
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.container}>
+        <GreenText
+          center
+          color={!loading && !postingSuccess ? colors.seekTeal : colors.seekiNatGreen}
+          text={headerText}
+        />
+        <View style={styles.margin} />
+        <View style={styles.center}>
+          {image}
+          <Text style={[styles.text, loading && styles.margin]}>
+            {extraText}
+          </Text>
+        </View>
+      </View>
       {!loading ? (
         <View style={styles.bottom}>
-          <TouchableOpacity
-            onPress={() => {
+          <GreenButton
+            color={!loading && !postingSuccess ? colors.seekTeal : null}
+            handlePress={() => {
               if ( postingSuccess ) {
                 navigation.goBack();
               } else {
                 togglePostModal();
               }
             }}
-            style={[
-              styles.greenButton,
-              ( !loading && !postingSuccess ) && { backgroundColor: colors.seekTeal }
-            ]}
-          >
-            <Text style={styles.buttonText}>
-              {i18n.t( "posting.ok" ).toLocaleUpperCase()}
-            </Text>
-          </TouchableOpacity>
+            text={i18n.t( "posting.ok" ).toLocaleUpperCase()}
+          />
         </View>
       ) : null}
-    </View>
+    </>
   );
 };
 
--- components/UIComponents/SafeAreaView.js
+++ components/UIComponents/SafeAreaView.js
@@ -1,8 +1,15 @@
+// @flow
 import React from "react";
 import { SafeAreaView } from "react-native";
 
 import styles from "../../styles/uiComponents/safeView";
 
-const SafeView = () => <SafeAreaView style={styles.safeViewTop} />;
+type Props = {
+  +color: ?string
+}
+
+const SafeView = ( { color }: Props ) => (
+  <SafeAreaView style={[styles.safeViewTop, color && { backgroundColor: color }]} />
+);
 
 export default SafeView;

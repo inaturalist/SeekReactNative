35833ed9dcd471090a3e2455e5b9575409b2b798
Amanda Bullington <albullington@gmail.com>
2019-10-14 13:26:57 -0700

    Update side menu to use hooks instead of class component

--- components/Home/SideMenu.js
+++ components/Home/SideMenu.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from "react";
+import React from "react";
 import {
   View,
   Image,
@@ -8,99 +8,90 @@ import {
   Text,
   SafeAreaView
 } from "react-native";
+import { useNavigation } from "react-navigation-hooks";
 
 import i18n from "../../i18n";
 import styles from "../../styles/home/sideMenu";
 import logoImages from "../../assets/logos";
 import icons from "../../assets/icons";
 
-type Props = {
-  +navigation: any
-}
+const SideMenu = () => {
+  const { navigate } = useNavigation();
 
-class SideMenu extends Component<Props> {
-  navigateTo( route ) {
-    const { navigation } = this.props;
-
-    navigation.navigate( route );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <SafeAreaView />
+  return (
+    <View style={styles.container}>
+      <SafeAreaView />
+      <TouchableOpacity
+        onPress={() => navigate( "Main" )}
+      >
+        <Image
+          source={logoImages.seek}
+          style={styles.logo}
+        />
+      </TouchableOpacity>
+      <View style={styles.textContainer}>
         <TouchableOpacity
-          onPress={() => this.navigateTo( "Main" )}
+          onPress={() => navigate( "Main" )}
+          style={[styles.row, styles.height]}
         >
-          <Image
-            source={logoImages.seek}
-            style={styles.logo}
-          />
+          <Image source={icons.menuHome} style={styles.image} />
+          <Text adjustsFontSizeToFit style={styles.text}>
+            {i18n.t( "menu.home" ).toLocaleUpperCase()}
+          </Text>
         </TouchableOpacity>
-        <View style={styles.textContainer}>
-          <TouchableOpacity
-            onPress={() => this.navigateTo( "Main" )}
-            style={[styles.row, styles.height]}
-          >
-            <Image source={icons.menuHome} style={styles.image} />
-            <Text adjustsFontSizeToFit style={styles.text}>
-              {i18n.t( "menu.home" ).toLocaleUpperCase()}
-            </Text>
-          </TouchableOpacity>
-          <View style={styles.divider} />
-          <TouchableOpacity
-            onPress={() => this.navigateTo( "Achievements" )}
-            style={[styles.row, styles.height]}
-          >
-            <Image source={icons.menuAchievements} style={styles.image} />
-            <Text adjustsFontSizeToFit style={styles.text}>
-              {i18n.t( "menu.achievements" ).toLocaleUpperCase()}
-            </Text>
-          </TouchableOpacity>
-          <View style={styles.divider} />
-          <TouchableOpacity
-            onPress={() => this.navigateTo( "Challenges" )}
-            style={[styles.row, styles.height]}
-          >
-            <Image source={icons.menuChallenges} style={styles.image} />
-            <Text adjustsFontSizeToFit style={styles.text}>
-              {i18n.t( "menu.challenges" ).toLocaleUpperCase()}
-            </Text>
-          </TouchableOpacity>
-          <View style={styles.divider} />
-          <TouchableOpacity
-            onPress={() => this.navigateTo( "MyObservations" )}
-            style={[styles.row, styles.height]}
-          >
-            <Image source={icons.menuObservations} style={styles.image} />
-            <Text adjustsFontSizeToFit style={styles.text}>
-              {i18n.t( "menu.observations" ).toLocaleUpperCase()}
-            </Text>
-          </TouchableOpacity>
-          <View style={styles.divider} />
-          <TouchableOpacity
-            onPress={() => this.navigateTo( "iNatStats" )}
-            style={[styles.row, styles.height]}
-          >
-            <Image source={icons.menuiNat} style={styles.image} />
-            <Text adjustsFontSizeToFit style={styles.text}>
-              {i18n.t( "menu.inat" ).toLocaleUpperCase()}
-            </Text>
-          </TouchableOpacity>
-          <View style={styles.divider} />
-          <TouchableOpacity
-            onPress={() => this.navigateTo( "About" )}
-            style={[styles.row, styles.height]}
-          >
-            <Image source={icons.menuSeek} style={styles.image} />
-            <Text adjustsFontSizeToFit style={styles.text}>
-              {i18n.t( "menu.about" ).toLocaleUpperCase()}
-            </Text>
-          </TouchableOpacity>
-        </View>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => navigate( "Achievements" )}
+          style={[styles.row, styles.height]}
+        >
+          <Image source={icons.menuAchievements} style={styles.image} />
+          <Text adjustsFontSizeToFit style={styles.text}>
+            {i18n.t( "menu.achievements" ).toLocaleUpperCase()}
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => navigate( "Challenges" )}
+          style={[styles.row, styles.height]}
+        >
+          <Image source={icons.menuChallenges} style={styles.image} />
+          <Text adjustsFontSizeToFit style={styles.text}>
+            {i18n.t( "menu.challenges" ).toLocaleUpperCase()}
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => navigate( "MyObservations" )}
+          style={[styles.row, styles.height]}
+        >
+          <Image source={icons.menuObservations} style={styles.image} />
+          <Text adjustsFontSizeToFit style={styles.text}>
+            {i18n.t( "menu.observations" ).toLocaleUpperCase()}
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => navigate( "iNatStats" )}
+          style={[styles.row, styles.height]}
+        >
+          <Image source={icons.menuiNat} style={styles.image} />
+          <Text adjustsFontSizeToFit style={styles.text}>
+            {i18n.t( "menu.inat" ).toLocaleUpperCase()}
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => navigate( "About" )}
+          style={[styles.row, styles.height]}
+        >
+          <Image source={icons.menuSeek} style={styles.image} />
+          <Text adjustsFontSizeToFit style={styles.text}>
+            {i18n.t( "menu.about" ).toLocaleUpperCase()}
+          </Text>
+        </TouchableOpacity>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 export default SideMenu;

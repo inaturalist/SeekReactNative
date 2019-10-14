304439773372dc5646b1f6fab9b873e233e88d31
Amanda Bullington <albullington@gmail.com>
2019-10-14 12:38:18 -0700

    Use hooks to avoid needing to pass navigation through props

--- components/LoginOrSignupScreen.js
+++ components/LoginOrSignupScreen.js
@@ -1,5 +1,3 @@
-// @flow
-
 import React from "react";
 import {
   View,
@@ -8,52 +6,53 @@ import {
   Image,
   ImageBackground
 } from "react-native";
+import { useNavigation } from "react-navigation-hooks";
 
 import i18n from "../i18n";
 import styles from "../styles/loginOrSignup";
 import logoImages from "../assets/logos";
 import backgrounds from "../assets/backgrounds";
 
-type Props = {
-  +navigation: any
-}
+const LoginOrSignupScreen = () => {
+  const { navigate } = useNavigation();
 
-const LoginOrSignupScreen = ( { navigation }: Props ) => (
-  <ImageBackground
-    source={backgrounds.splash}
-    style={styles.container}
-  >
-    <Image source={logoImages.seek} style={styles.logo} />
-    <View style={styles.buttonContainer}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate( "LoginScreen" )}
-        style={styles.whiteButton}
-      >
-        <Text style={styles.buttonText}>
-          {i18n.t( "login.log_in" ).toLocaleUpperCase()}
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate( "Age" )}
-        style={styles.whiteButton}
-      >
-        <Text style={styles.buttonText}>
-          {i18n.t( "login.sign_up" ).toLocaleUpperCase()}
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate( "Main" )}
-        style={[styles.whiteButton, { height: 79, borderRadius: 100 }]}
-      >
-        <Text style={[styles.buttonText, { lineHeight: 24 }]}>
-          {i18n.t( "login.skip_login" ).toLocaleUpperCase()}
-        </Text>
-      </TouchableOpacity>
-    </View>
-    <Text style={styles.text}>
-      {i18n.t( "login.about" )}
-    </Text>
-  </ImageBackground>
-);
+  return (
+    <ImageBackground
+      source={backgrounds.splash}
+      style={styles.container}
+    >
+      <Image source={logoImages.seek} style={styles.logo} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => navigate( "LoginScreen" )}
+          style={styles.whiteButton}
+        >
+          <Text style={styles.buttonText}>
+            {i18n.t( "login.log_in" ).toLocaleUpperCase()}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigate( "Age" )}
+          style={styles.whiteButton}
+        >
+          <Text style={styles.buttonText}>
+            {i18n.t( "login.sign_up" ).toLocaleUpperCase()}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigate( "Main" )}
+          style={[styles.whiteButton, { height: 79, borderRadius: 100 }]}
+        >
+          <Text style={[styles.buttonText, { lineHeight: 24 }]}>
+            {i18n.t( "login.skip_login" ).toLocaleUpperCase()}
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.text}>
+        {i18n.t( "login.about" )}
+      </Text>
+    </ImageBackground>
+  );
+};
 
 export default LoginOrSignupScreen;

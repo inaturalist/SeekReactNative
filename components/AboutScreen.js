f50ad63a513c1b0f75a22d35a7e19ed8e6b00429
Amanda Bullington <albullington@gmail.com>
2019-10-30 13:51:30 -0700

    Attempt to fix Android album picker crash

--- components/AboutScreen.js
+++ components/AboutScreen.js
@@ -31,7 +31,7 @@ class AboutScreen extends Component<Props> {
   render() {
     const { navigation } = this.props;
     const version = "2.3.7";
-    const buildNumber = 68;
+    const buildNumber = 69;
 
     return (
       <React.Fragment>

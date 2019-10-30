a84109acff352f4ed6cf3f9ae8f311071f6c8146
Amanda Bullington <albullington@gmail.com>
2019-10-30 12:28:45 -0700

    Updated version to 2.3.7-68 for beta testing

--- components/AboutScreen.js
+++ components/AboutScreen.js
@@ -30,8 +30,8 @@ class AboutScreen extends Component<Props> {
 
   render() {
     const { navigation } = this.props;
-    const version = "2.3.6";
-    const buildNumber = 67;
+    const version = "2.3.7";
+    const buildNumber = 68;
 
     return (
       <React.Fragment>

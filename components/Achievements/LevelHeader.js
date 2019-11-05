6cadfae528ff243539cde5fb4f8edf3a56c175c6
Amanda Bullington <albullington@gmail.com>
2019-11-05 14:55:56 -0800

    Moved level modal logic into level header component with hooks

--- components/Achievements/LevelHeader.js
+++ components/Achievements/LevelHeader.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Image,
@@ -8,7 +8,9 @@ import {
   TouchableOpacity
 } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
+import Modal from "react-native-modal";
 
+import LevelModal from "../AchievementModals/LevelModal";
 import i18n from "../../i18n";
 import badgeImages from "../../assets/badges";
 import styles from "../../styles/badges/badges";
@@ -16,33 +18,54 @@ import styles from "../../styles/badges/badges";
 type Props = {
   +level: Object,
   +nextLevelCount: number,
-  +toggleLevelModal: Function
+  +speciesCount: number
 }
 
-const LevelHeader = ( { level, nextLevelCount, toggleLevelModal }: Props ) => (
-  <TouchableOpacity
-    onPress={() => toggleLevelModal()}
-  >
-    <LinearGradient
-      colors={["#22784d", "#38976d"]}
-      style={[styles.header, styles.center]}
+const LevelHeader = ( { level, nextLevelCount, speciesCount }: Props ) => {
+  const [showLevelModal, setLevelModal] = useState( false );
+
+  const toggleLevelModal = () => {
+    setLevelModal( !showLevelModal );
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={() => toggleLevelModal()}
     >
-      {level ? (
-        <View style={styles.row}>
-          <Image source={badgeImages[level.earnedIconName]} style={styles.levelImage} />
-          <View style={styles.textContainer}>
-            <Text style={styles.lightText}>{i18n.t( "badges.your_level" ).toLocaleUpperCase()}</Text>
-            <Text style={styles.headerText}>{i18n.t( level.intlName ).toLocaleUpperCase()}</Text>
-            <Text style={styles.text}>
-              {level.count >= 150
-                ? i18n.t( "badges.observe_max" )
-                : i18n.t( "badges.observe", { number: nextLevelCount } )}
-            </Text>
+      <Modal
+        isVisible={showLevelModal}
+        onBackdropPress={() => toggleLevelModal()}
+        onSwipeComplete={() => toggleLevelModal()}
+        swipeDirection="down"
+      >
+        <LevelModal
+          level={level}
+          screen="achievements"
+          speciesCount={speciesCount}
+          toggleLevelModal={toggleLevelModal}
+        />
+      </Modal>
+      <LinearGradient
+        colors={["#22784d", "#38976d"]}
+        style={[styles.header, styles.center]}
+      >
+        {level ? (
+          <View style={styles.row}>
+            <Image source={badgeImages[level.earnedIconName]} style={styles.levelImage} />
+            <View style={styles.textContainer}>
+              <Text style={styles.lightText}>{i18n.t( "badges.your_level" ).toLocaleUpperCase()}</Text>
+              <Text style={styles.headerText}>{i18n.t( level.intlName ).toLocaleUpperCase()}</Text>
+              <Text style={styles.text}>
+                {level.count >= 150
+                  ? i18n.t( "badges.observe_max" )
+                  : i18n.t( "badges.observe", { number: nextLevelCount } )}
+              </Text>
+            </View>
           </View>
-        </View>
-      ) : null}
-    </LinearGradient>
-  </TouchableOpacity>
-);
+        ) : null}
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
 
 export default LevelHeader;

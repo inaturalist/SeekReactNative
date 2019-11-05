0909783247c0bd8a02a4335e4914552385787059
Amanda Bullington <albullington@gmail.com>
2019-11-05 14:01:40 -0800

    Add accessibility label to modal back button

--- components/UIComponents/ModalBackButton.js
+++ components/UIComponents/ModalBackButton.js
@@ -6,6 +6,7 @@ import {
   Image
 } from "react-native";
 
+import i18n from "../../i18n";
 import styles from "../../styles/uiComponents/modalBackButton";
 import icons from "../../assets/icons";
 
@@ -14,7 +15,12 @@ type Props = {
 }
 
 const ModalBackButton = ( { toggleModal }: Props ) => (
-  <TouchableOpacity onPress={() => toggleModal()} style={styles.backButton}>
+  <TouchableOpacity
+    accessibilityLabel={i18n.t( "accessibility.back" )}
+    accessible
+    onPress={() => toggleModal()}
+    style={styles.backButton}
+  >
     <Image source={icons.closeModal} />
   </TouchableOpacity>
 );

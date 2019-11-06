fb0e54962cba834b958f12386d69a15c0ea91e6a
Amanda Bullington <albullington@gmail.com>
2019-11-05 16:26:26 -0800

    Updated about screen to use hooks and useRef

--- components/AboutScreen.js
+++ components/AboutScreen.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from "react";
+import React, { useRef } from "react";
 import {
   Image,
   ScrollView,
@@ -21,67 +21,65 @@ type Props = {
   +navigation: any
 };
 
-class AboutScreen extends Component<Props> {
-  scrollToTop() {
-    this.scrollView.scrollTo( {
-      x: 0, y: 0, animated: Platform.OS === "android"
-    } );
-  }
+const AboutScreen = ( { navigation }: Props ) => {
+  const scrollViewRef = useRef( null );
+  const version = "2.3.7";
+  const buildNumber = 70;
 
-  render() {
-    const { navigation } = this.props;
-    const version = "2.3.7";
-    const buildNumber = 70;
+  const scrollToTop = () => {
+    if ( scrollViewRef ) {
+      scrollViewRef.current.scrollTo( {
+        x: 0, y: 0, animated: Platform.OS === "android"
+      } );
+    }
+  };
 
-    return (
-      <React.Fragment>
-        <SafeAreaView />
-        <NavigationEvents
-          onWillFocus={() => {
-            this.scrollToTop();
-          }}
-        />
-        <GreenHeader header={i18n.t( "about.header" )} navigation={navigation} />
-        <ScrollView
-          ref={( ref ) => { this.scrollView = ref; }}
-          contentContainerStyle={styles.textContainer}
-        >
-          <View style={styles.row}>
-            <Image source={logos.opBlack} />
-            <Image source={logos.wwf} style={{ marginLeft: 20 }} />
-          </View>
-          <View style={styles.margin} />
-          <Text style={styles.boldText}>{i18n.t( "about.sponsored" )}</Text>
-          <Text style={styles.text}>{i18n.t( "about.our_planet" )}</Text>
-          <View style={styles.block} />
-          <Image source={logos.iNat} />
-          <View style={styles.margin} />
-          <Text style={styles.boldText}>{i18n.t( "about.seek" )}</Text>
-          <Text style={styles.text}>{i18n.t( "about.joint_initiative" )}</Text>
-          <View style={styles.block} />
-          <Image source={logos.casNatGeo} style={styles.image} />
-          <View style={styles.margin} />
-          <Text style={styles.text}>{i18n.t( "about.original" )}</Text>
-          <View style={styles.margin} />
-          <Image source={logos.hhmi} />
-          <View style={styles.block} />
-          <Text style={styles.boldText}>{i18n.t( "about.designed_by" )}</Text>
-          <Text style={styles.text}>{i18n.t( "about.inat_team" )}</Text>
-          <View style={styles.block} />
-          <Text style={styles.greenText}>
-            {i18n.t( "about.version" ).toLocaleUpperCase()}
-            {` ${version} (${buildNumber})`}
-          </Text>
-          <View style={styles.block} />
-          <Text style={styles.text}>
-            {i18n.t( "about.help" )}
-          </Text>
-          <View style={styles.block} />
-          <Padding />
-        </ScrollView>
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <SafeAreaView />
+      <NavigationEvents
+        onWillBlur={() => scrollToTop()}
+      />
+      <GreenHeader header={i18n.t( "about.header" )} navigation={navigation} />
+      <ScrollView
+        ref={scrollViewRef}
+        contentContainerStyle={styles.textContainer}
+      >
+        <View style={styles.row}>
+          <Image source={logos.opBlack} />
+          <Image source={logos.wwf} style={{ marginLeft: 20 }} />
+        </View>
+        <View style={styles.margin} />
+        <Text style={styles.boldText}>{i18n.t( "about.sponsored" )}</Text>
+        <Text style={styles.text}>{i18n.t( "about.our_planet" )}</Text>
+        <View style={styles.block} />
+        <Image source={logos.iNat} />
+        <View style={styles.margin} />
+        <Text style={styles.boldText}>{i18n.t( "about.seek" )}</Text>
+        <Text style={styles.text}>{i18n.t( "about.joint_initiative" )}</Text>
+        <View style={styles.block} />
+        <Image source={logos.casNatGeo} style={styles.image} />
+        <View style={styles.margin} />
+        <Text style={styles.text}>{i18n.t( "about.original" )}</Text>
+        <View style={styles.margin} />
+        <Image source={logos.hhmi} />
+        <View style={styles.block} />
+        <Text style={styles.boldText}>{i18n.t( "about.designed_by" )}</Text>
+        <Text style={styles.text}>{i18n.t( "about.inat_team" )}</Text>
+        <View style={styles.block} />
+        <Text style={styles.greenText}>
+          {i18n.t( "about.version" ).toLocaleUpperCase()}
+          {` ${version} (${buildNumber})`}
+        </Text>
+        <View style={styles.block} />
+        <Text style={styles.text}>
+          {i18n.t( "about.help" )}
+        </Text>
+        <View style={styles.block} />
+        <Padding />
+      </ScrollView>
+    </React.Fragment>
+  );
+};
 
 export default AboutScreen;

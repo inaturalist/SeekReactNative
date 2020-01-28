26c4000f5dc1b0ecbb252bb16f3cdeac816e71d2
Amanda Bullington <albullington@gmail.com>
2020-01-28 09:58:40 -0800

    Add styling for debug screen

---
+++ styles/uiComponents/debugAndroid.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import {
+  colors,
+  center
+} from "../global";
+
+export default StyleSheet.create( {
+  background: {
+    backgroundColor: colors.white,
+    flex: 1
+  },
+  center
+} );

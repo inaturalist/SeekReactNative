c2e4ea547749c13c98e596ac50614b6c57163509
Amanda Bullington <albullington@gmail.com>
2020-03-27 17:25:30 -0700

    Updated build to 104 for release build of 2.5.0

--- utility/challengeHelpers.js
+++ utility/challengeHelpers.js
@@ -177,9 +177,9 @@ const setupChallenges = () => {
 
         dict.forEach( ( challengesType ) => {
           const challenges = challengesDict[challengesType];
-          // const isAvailable = checkIfChallengeAvailable( challenges.availableDate );
+          const isAvailable = checkIfChallengeAvailable( challenges.availableDate );
 
-          // if ( isAvailable ) {
+          if ( isAvailable ) {
             realm.create( "ChallengeRealm", {
               name: challenges.name,
               description: challenges.description,
@@ -192,7 +192,7 @@ const setupChallenges = () => {
               action: challenges.action,
               index: challenges.index
             }, true );
-          // }
+          }
         } );
       } );
     } ).catch( ( err ) => {

10933eee6e6b810d5f5c793a6f03d7f3ebfbc52a
Amanda Bullington <albullington@gmail.com>
2020-05-26 11:43:33 -0700

    Remove unneeded assets

--- assets/posting.js
+++ assets/posting.js
@@ -2,14 +2,10 @@ const posting = {
   captive: require( "./posting/icon-captive.png" ),
   crosshair: require( "./posting/icon-crosshair.png" ),
   date: require( "./posting/icon-date.png" ),
-  // error: require( "./posting/icon-error-inatgreen.png" ),
-  // expand: require( "./posting/icon-expand.png" ),
   geoprivacy: require( "./posting/icon-geoprivacy.png" ),
   bird: require( "./logos/iNatStatsSpeciesDetail-Bird.png" ),
   location: require( "./posting/icon-location.png" ),
-  // search: require( "./posting/icon-search.png" ),
   searchGreen: require( "./posting/icon-search-green.png" ),
-  // uploadfail: require( "./posting/icon-uploadfail.png" ),
   postingHelp: require( "./posting/icon-help.png" )
 };
 

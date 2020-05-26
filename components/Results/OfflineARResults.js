f01426571353298a59bea9b567dfc0805edf9446
Amanda Bullington <albullington@gmail.com>
2020-05-26 16:21:57 -0700

    Update offline results screen to use hooks

--- components/Results/OfflineARResults.js
+++ components/Results/OfflineARResults.js
@@ -1,89 +1,75 @@
 // @flow
 
-import React, { Component } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Platform } from "react-native";
 import inatjs from "inaturalistjs";
-import { NavigationEvents } from "@react-navigation/compat";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
-import {
-  getTaxonCommonName,
-  checkForIconicTaxonId
-} from "../../utility/helpers";
+import { getTaxonCommonName, checkForIconicTaxonId } from "../../utility/helpers";
 import { addToCollection } from "../../utility/observationHelpers";
 import FullPhotoLoading from "./FullPhotoLoading";
 import { fetchTruncatedUserLocation } from "../../utility/locationHelpers";
-import { checkLocationPermissions } from "../../utility/androidHelpers.android";
 import createUserAgent from "../../utility/userAgent";
 import { fetchSpeciesSeenDate } from "../../utility/dateHelpers";
+import { useLocationPermission } from "../../utility/customHooks";
 
 const threshold = 0.7;
 
-type Props = {
-  +route: any,
-  +navigation: any
-}
+const OfflineARResults = () => {
+  const granted = useLocationPermission();
+  const navigation = useNavigation();
+  const { params } = useRoute();
 
-type State = {
-  image: Object,
-  taxon: Object,
-  observation: Object,
-  seenDate: ?string,
-  match: ?boolean,
-  errorCode: ?number
-};
+  const [taxon, setTaxon] = useState( {} );
+  const [image, setImage] = useState( params.image );
+  const [observation, setObservation] = useState( null );
+  const [seenDate, setSeenDate] = useState( null );
+  const [match, setMatch] = useState( null );
+  const [errorCode, setLocationErrorCode] = useState( null );
 
-class OfflineARResults extends Component<Props, State> {
-  constructor( { route }: Props ) {
-    super();
+  const setSpeciesInfo = ( species, taxa ) => {
+    const taxaId = Number( species.taxon_id );
+    const iconicTaxonId = checkForIconicTaxonId( species.ancestor_ids );
 
-    const { image } = route.params;
+    getTaxonCommonName( species.taxon_id ).then( ( commonName ) => {
+      setObservation( {
+        taxon: {
+          default_photo: taxa && taxa.default_photo ? taxa.default_photo : null,
+          id: taxaId,
+          name: species.name,
+          preferred_common_name: commonName,
+          iconic_taxon_id: iconicTaxonId,
+          ancestor_ids: species.ancestor_ids
+        }
+      } );
 
-    this.state = {
-      taxon: {},
-      image,
-      observation: null,
-      seenDate: null,
-      match: null,
-      errorCode: null
-    };
-  }
+      const newTaxon = {
+        taxaId,
+        taxaName: commonName || species.name,
+        scientificName: species.name,
+        speciesSeenImage:
+          taxa && taxa.taxon_photos[0]
+            ? taxa.taxon_photos[0].photo.medium_url
+            : null
+      };
 
-  setLocationErrorCode( errorCode: number ) {
-    this.setState( { errorCode } );
-  }
-
-  getUserLocation() {
-    const { image } = this.state;
-    fetchTruncatedUserLocation().then( ( coords ) => {
-      if ( coords ) {
-        const { latitude, longitude } = coords;
-
-        image.latitude = latitude;
-        image.longitude = longitude;
-
-        this.setState( { image }, () => this.setARCameraVisionResults() );
-      } else {
-        this.setARCameraVisionResults();
-      }
-    } ).catch( ( errorCode ) => {
-      this.setLocationErrorCode( errorCode );
-      this.setARCameraVisionResults();
+      setTaxon( newTaxon );
+      setMatch( true );
     } );
-  }
+  };
 
-  setSeenDate( seenDate: string ) {
-    this.setState( { seenDate } );
-  }
+  const fetchAdditionalSpeciesInfo = useCallback( ( species ) => {
+    const options = { user_agent: createUserAgent() };
 
-  setMatch( match: boolean ) {
-    this.setState( { match }, () => this.showMatch() );
-  }
+    inatjs.taxa.fetch( species.taxon_id, options ).then( ( response ) => {
+      const taxa = response.results[0];
+      setSpeciesInfo( species, taxa );
+    } ).catch( () => {
+      setSpeciesInfo( species );
+    } );
+  }, [] );
 
-  setTaxon( taxon: Object, match: boolean ) {
-    this.setState( { taxon }, () => this.setMatch( match ) );
-  }
-
-  setCommonAncestor( ancestor: Object, speciesSeenImage: ?string ) {
+  const setCommonAncestor = ( ancestor, speciesSeenImage ) => {
     getTaxonCommonName( ancestor.taxon_id ).then( ( commonName ) => {
       const newTaxon = {
         commonAncestor: commonName || ancestor.name,
@@ -93,13 +79,42 @@ class OfflineARResults extends Component<Props, State> {
         rank: ancestor.rank
       };
 
-      this.setTaxon( newTaxon, false );
+      setTaxon( newTaxon );
+      setMatch( false );
     } );
-  }
+  };
 
-  setARCameraVisionResults() {
-    const { image } = this.state;
+  const fetchAdditionalAncestorInfo = useCallback( ( ancestor ) => {
+    const options = { user_agent: createUserAgent() };
 
+    inatjs.taxa.fetch( ancestor.taxon_id, options ).then( ( response ) => {
+      const taxa = response.results[0];
+      const speciesSeenImage = taxa.taxon_photos[0] ? taxa.taxon_photos[0].photo.medium_url : null;
+      setCommonAncestor( ancestor, speciesSeenImage );
+    } ).catch( () => {
+      setCommonAncestor( ancestor );
+    } );
+  }, [] );
+
+  const checkForCommonAncestor = useCallback( () => {
+    const reversePredictions = image.predictions.reverse();
+
+    const ancestor = reversePredictions.find( leaf => leaf.score > threshold );
+
+    if ( ancestor && ancestor.rank !== 100 ) {
+      fetchAdditionalAncestorInfo( ancestor );
+    } else {
+      setMatch( false );
+    }
+  }, [fetchAdditionalAncestorInfo, image.predictions] );
+
+  const checkSpeciesSeen = ( taxaId ) => {
+    fetchSpeciesSeenDate( taxaId ).then( ( date ) => {
+      setSeenDate( date );
+    } );
+  };
+
+  const setARCameraVisionResults = useCallback( () => {
     const ancestorIds = [];
 
     if ( Platform.OS === "ios" ) {
@@ -115,136 +130,51 @@ class OfflineARResults extends Component<Props, State> {
       if ( Platform.OS === "ios" ) {
         species.ancestor_ids = ancestorIds.sort();
       }
-      this.checkSpeciesSeen( Number( species.taxon_id ) );
-      this.fetchAdditionalSpeciesInfo( species );
+      checkSpeciesSeen( Number( species.taxon_id ) );
+      fetchAdditionalSpeciesInfo( species );
     } else {
-      this.checkForCommonAncestor();
+      checkForCommonAncestor();
     }
-  }
+  }, [checkForCommonAncestor, fetchAdditionalSpeciesInfo, image.predictions] );
 
-  setSpeciesInfo( species: Object, taxa: Object ) {
-    const taxaId = Number( species.taxon_id );
+  const getUserLocation = useCallback( () => {
+    fetchTruncatedUserLocation().then( ( coords ) => {
+      if ( coords ) {
+        const { latitude, longitude } = coords;
 
-    const iconicTaxonId = checkForIconicTaxonId( species.ancestor_ids );
+        image.latitude = latitude;
+        image.longitude = longitude;
 
-    getTaxonCommonName( species.taxon_id ).then( ( commonName ) => {
-      this.setState( {
-        observation: {
-          taxon: {
-            default_photo: taxa && taxa.default_photo ? taxa.default_photo : null,
-            id: taxaId,
-            name: species.name,
-            preferred_common_name: commonName,
-            iconic_taxon_id: iconicTaxonId,
-            ancestor_ids: species.ancestor_ids
-          }
-        }
-      } );
-
-      const newTaxon = {
-        taxaId,
-        taxaName: commonName || species.name,
-        scientificName: species.name,
-        speciesSeenImage:
-          taxa && taxa.taxon_photos[0]
-            ? taxa.taxon_photos[0].photo.medium_url
-            : null
-      };
-
-      this.setTaxon( newTaxon, true );
-    } );
-  }
-
-  async showMatch() {
-    const { seenDate, match } = this.state;
-
-    if ( !seenDate && match ) {
-      await this.addObservation();
-      this.navigateToMatch();
-    } else {
-      this.navigateToMatch();
-    }
-  }
-
-  fetchAdditionalSpeciesInfo( species: Object ) {
-    const options = { user_agent: createUserAgent() };
-
-    inatjs.taxa.fetch( species.taxon_id, options ).then( ( response ) => {
-      const taxa = response.results[0];
-      this.setSpeciesInfo( species, taxa );
-    } ).catch( () => {
-      this.setSpeciesInfo( species );
-    } );
-  }
-
-  fetchAdditionalAncestorInfo( ancestor: Object ) {
-    const options = { user_agent: createUserAgent() };
-
-    inatjs.taxa.fetch( ancestor.taxon_id, options ).then( ( response ) => {
-      const taxa = response.results[0];
-      const speciesSeenImage = taxa.taxon_photos[0] ? taxa.taxon_photos[0].photo.medium_url : null;
-      this.setCommonAncestor( ancestor, speciesSeenImage );
-    } ).catch( () => {
-      this.setCommonAncestor( ancestor );
-    } );
-  }
-
-  checkForCommonAncestor() {
-    const { image } = this.state;
-    const reversePredictions = image.predictions.reverse();
-
-    const ancestor = reversePredictions.find( leaf => leaf.score > threshold );
-
-    if ( ancestor && ancestor.rank !== 100 ) {
-      this.fetchAdditionalAncestorInfo( ancestor );
-    } else {
-      this.setMatch( false );
-    }
-  }
-
-  addObservation() {
-    const { image, observation } = this.state;
-
-    addToCollection( observation, image );
-  }
-
-  checkSpeciesSeen( taxaId: number ) {
-    fetchSpeciesSeenDate( taxaId ).then( ( date ) => {
-      this.setSeenDate( date );
-    } );
-  }
-
-  requestAndroidPermissions() {
-    const { image } = this.state;
-
-    if ( !image.latitude || !image.longitude ) {
-      // Android photo gallery images should already have lat/lng
-      if ( Platform.OS === "android" ) {
-        checkLocationPermissions().then( ( granted ) => {
-          if ( granted ) {
-            this.getUserLocation();
-          } else {
-            this.setLocationErrorCode( 1 );
-            this.setARCameraVisionResults();
-          }
-        } );
+        setImage( image );
+        setARCameraVisionResults();
       } else {
-        this.getUserLocation();
+        setARCameraVisionResults();
+      }
+    } ).catch( ( code ) => {
+      setLocationErrorCode( code );
+      setARCameraVisionResults();
+    } );
+  }, [image, setARCameraVisionResults] );
+
+  const requestAndroidPermissions = useCallback( () => {
+    if ( !image.latitude ) {
+      // Android photo gallery images should already have lat/lng
+      if ( Platform.OS === "android" && !granted ) {
+        setLocationErrorCode( 1 );
+        setARCameraVisionResults();
+      } else {
+        getUserLocation();
       }
     } else {
-      this.setARCameraVisionResults();
+      setARCameraVisionResults();
     }
-  }
+  }, [image.latitude, granted, getUserLocation, setARCameraVisionResults] );
 
-  navigateToMatch() {
-    const { navigation } = this.props;
-    const {
-      taxon,
-      image,
-      seenDate,
-      errorCode
-    } = this.state;
+  const addObservation = useCallback( async () => {
+    await addToCollection( observation, image );
+  }, [observation, image] );
 
+  const navigateToMatch = useCallback( () => {
     navigation.push( "Drawer", {
       screen: "Main",
       params: {
@@ -257,22 +187,30 @@ class OfflineARResults extends Component<Props, State> {
         }
       }
     } );
-  }
+  }, [taxon, image, seenDate, errorCode, navigation] );
 
-  render() {
-    const { image } = this.state;
+  const showMatch = useCallback( async () => {
+    if ( !seenDate && match ) {
+      await addObservation();
+      navigateToMatch();
+    } else {
+      navigateToMatch();
+    }
+  }, [navigateToMatch, seenDate, match, addObservation] );
 
-    return (
-      <>
-        <NavigationEvents
-          onWillFocus={() => {
-            this.requestAndroidPermissions();
-          }}
-        />
-        <FullPhotoLoading uri={image.uri} />
-      </>
-    );
-  }
-}
+  useEffect( () => {
+    if ( match !== null ) {
+      showMatch();
+    }
+  }, [match, showMatch] );
+
+  useEffect( () => {
+    navigation.addListener( "focus", () => {
+      requestAndroidPermissions();
+    } );
+  }, [navigation, requestAndroidPermissions] );
+
+  return <FullPhotoLoading uri={image.uri} />;
+};
 
 export default OfflineARResults;

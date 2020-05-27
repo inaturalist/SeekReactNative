dd41b7ee310591af4c20c16cb7e72e12f42b0897
Amanda Bullington <albullington@gmail.com>
2020-05-26 17:03:37 -0700

    Update Online Server results to use hooks & update safe area view in confirm screen

--- components/Results/OnlineServerResults.js
+++ components/Results/OnlineServerResults.js
@@ -1,9 +1,8 @@
 // @flow
 
-import React, { Component } from "react";
-import { Platform } from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
 import inatjs from "inaturalistjs";
-import { NavigationEvents } from "@react-navigation/compat";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 import ConfirmScreen from "./ConfirmScreen";
 import ErrorScreen from "./Error";
@@ -15,50 +14,24 @@ import {
 } from "../../utility/helpers";
 import { addToCollection } from "../../utility/observationHelpers";
 import { fetchTruncatedUserLocation } from "../../utility/locationHelpers";
-import { checkLocationPermissions } from "../../utility/androidHelpers.android";
 import createUserAgent from "../../utility/userAgent";
 import { fetchSpeciesSeenDate, serverBackOnlineTime } from "../../utility/dateHelpers";
 
-type Props = {
-  +route: any,
-  +navigation: any
-}
+const OnlineServerResults = () => {
+  const navigation = useNavigation();
+  const { params } = useRoute();
 
-type State = {
-  image: Object,
-  taxon: Object,
-  observation: ?Object,
-  seenDate: ?string,
-  error: ?string,
-  match: ?boolean,
-  clicked: boolean,
-  numberOfHours: ?string,
-  errorCode: ?number
-};
+  const [taxon, setTaxon] = useState( {} );
+  const [image, setImage] = useState( params.image );
+  const [observation, setObservation] = useState( null );
+  const [seenDate, setSeenDate] = useState( null );
+  const [match, setMatch] = useState( null );
+  const [errorCode, setLocationErrorCode] = useState( null );
+  const [error, setError] = useState( null );
+  const [clicked, setClicked] = useState( false );
+  const [numberOfHours, setNumberOfHours] = useState( null );
 
-class OnlineServerResults extends Component<Props, State> {
-  constructor( { route }: Props ) {
-    super();
-
-    const { image } = route.params;
-
-    this.state = {
-      taxon: {},
-      image,
-      observation: null,
-      seenDate: null,
-      error: null,
-      match: null,
-      clicked: false,
-      numberOfHours: null,
-      errorCode: null
-    };
-
-    ( this:any ).checkForMatches = this.checkForMatches.bind( this );
-  }
-
-  getUserLocation() {
-    const { image } = this.state;
+  const getUserLocation = useCallback( () => {
     fetchTruncatedUserLocation().then( ( coords ) => {
       if ( coords ) {
         const { latitude, longitude } = coords;
@@ -66,47 +39,24 @@ class OnlineServerResults extends Component<Props, State> {
         image.latitude = latitude;
         image.longitude = longitude;
 
-        this.setState( { image } );
+        setImage( image );
       }
-    } ).catch( ( errorCode ) => {
-      this.setLocationErrorCode( errorCode );
+    } ).catch( ( code ) => {
+      setLocationErrorCode( code );
     } );
-  }
+  }, [image] );
 
-  setMatch( match: boolean ) {
-    const { clicked } = this.state;
-    this.setState( { match }, () => {
-      if ( clicked ) {
-        this.checkForMatches();
-      }
+  const checkSpeciesSeen = ( taxaId ) => {
+    fetchSpeciesSeenDate( taxaId ).then( ( date ) => {
+      setSeenDate( date );
     } );
-  }
+  };
 
-  setSeenDate( seenDate: ?string ) {
-    this.setState( { seenDate } );
-  }
-
-  setNumberOfHours( numberOfHours: string ) {
-    this.setState( { numberOfHours } );
-  }
-
-  setError( error: string ) {
-    this.setState( { error } );
-  }
-
-  setLocationErrorCode( errorCode: number ) {
-    this.setState( { errorCode } );
-  }
-
-  setTaxon( taxon: Object, match: boolean ) {
-    this.setState( { taxon }, () => this.setMatch( match ) );
-  }
-
-  setOnlineVisionSpeciesResults( species: Object ) {
+  const setOnlineVisionSpeciesResults = ( species ) => {
     const { taxon } = species;
     const photo = taxon.default_photo;
 
-    this.setState( { observation: species } );
+    setObservation( species );
 
     getTaxonCommonName( taxon.id ).then( ( commonName ) => {
       const newTaxon = {
@@ -116,11 +66,12 @@ class OnlineServerResults extends Component<Props, State> {
         speciesSeenImage: photo ? photo.medium_url : null
       };
 
-      this.setTaxon( newTaxon, true );
+      setTaxon( newTaxon );
+      setMatch( true );
     } );
-  }
+  };
 
-  setOnlineVisionAncestorResults( commonAncestor: Object ) {
+  const setOnlineVisionAncestorResults = ( commonAncestor ) => {
     const { taxon } = commonAncestor;
     const photo = taxon.default_photo;
 
@@ -135,33 +86,12 @@ class OnlineServerResults extends Component<Props, State> {
         rank: taxon.rank_level
       };
 
-      this.setTaxon( newTaxon, false );
+      setTaxon( newTaxon );
+      setMatch( false );
     } );
-  }
+  };
 
-  async getParamsForOnlineVision() {
-    const { image } = this.state;
-
-    const params = await flattenUploadParameters( image );
-    this.fetchScore( params );
-  }
-
-  async showMatch() {
-    const { seenDate } = this.state;
-
-    if ( !seenDate ) {
-      await this.addObservation();
-      this.navigateToMatch();
-    } else {
-      this.navigateToMatch();
-    }
-  }
-
-  showNoMatch() {
-    this.navigateToMatch();
-  }
-
-  fetchScore( params: Object ) {
+  const fetchScore = useCallback( ( params ) => {
     const token = createJwtToken();
 
     const options = { api_token: token, user_agent: createUserAgent() };
@@ -172,12 +102,12 @@ class OnlineServerResults extends Component<Props, State> {
         const commonAncestor = response.common_ancestor;
 
         if ( species.combined_score > 85 && species.taxon.rank === "species" ) {
-          this.checkSpeciesSeen( species.taxon.id );
-          this.setOnlineVisionSpeciesResults( species );
+          checkSpeciesSeen( species.taxon.id );
+          setOnlineVisionSpeciesResults( species );
         } else if ( commonAncestor ) {
-          this.setOnlineVisionAncestorResults( commonAncestor );
+          setOnlineVisionAncestorResults( commonAncestor );
         } else {
-          this.setMatch( false );
+          setMatch( false );
         }
       } ).catch( ( { response } ) => {
         if ( response.status && response.status === 503 ) {
@@ -186,71 +116,38 @@ class OnlineServerResults extends Component<Props, State> {
           const hours = serverBackOnlineTime( gmtTime );
 
           if ( hours ) {
-            this.setNumberOfHours( hours );
+            setNumberOfHours( hours );
           }
-          this.setError( "downtime" );
+          setError( "downtime" );
         } else {
-          this.setError( "onlineVision" );
+          setError( "onlineVision" );
         }
       } );
-  }
+  }, [] );
 
-  addObservation() {
-    const { image, observation } = this.state;
+  const addObservation = useCallback( async () => {
+    await addToCollection( observation, image );
+  }, [observation, image] );
 
-    addToCollection( observation, image );
-  }
+  const getParamsForOnlineVision = useCallback( async () => {
+    const uploadParams = await flattenUploadParameters( image );
+    fetchScore( uploadParams );
+  }, [fetchScore, image] );
 
-  checkSpeciesSeen( taxaId: number ) {
-    fetchSpeciesSeenDate( taxaId ).then( ( date ) => {
-      this.setSeenDate( date );
-    } );
-  }
+  const checkForMatches = () => setClicked( true );
 
-  checkForMatches() {
-    const { match } = this.state;
-
-    this.setState( { clicked: true } );
-
-    if ( match === true ) {
-      this.showMatch();
-    } else if ( match === false ) {
-      this.showNoMatch();
-    }
-  }
-
-  requestAndroidPermissions() {
+  const checkMetaData = useCallback( () => {
     // this should only apply to iOS photos with no metadata
     // once metadata is fixed, should be able to remove this check for user location
-    const { image } = this.state;
-
-    if ( !image.latitude || !image.longitude ) {
-      // check to see if there are already photo coordinates
-      if ( Platform.OS === "android" ) {
-        checkLocationPermissions().then( ( granted ) => {
-          if ( granted ) {
-            this.getUserLocation();
-            this.getParamsForOnlineVision();
-          }
-        } );
-      } else {
-        this.getUserLocation();
-        this.getParamsForOnlineVision();
-      }
+    if ( !image.latitude ) {
+      getUserLocation();
+      getParamsForOnlineVision();
     } else {
-      this.getParamsForOnlineVision();
+      getParamsForOnlineVision();
     }
-  }
+  }, [image.latitude, getParamsForOnlineVision, getUserLocation] );
 
-  navigateToMatch() {
-    const { navigation } = this.props;
-    const {
-      image,
-      taxon,
-      seenDate,
-      errorCode
-    } = this.state;
-
+  const navigateToMatch = useCallback( () => {
     navigation.push( "Drawer", {
       screen: "Main",
       params: {
@@ -263,37 +160,45 @@ class OnlineServerResults extends Component<Props, State> {
         }
       }
     } );
-  }
+  }, [taxon, image, seenDate, errorCode, navigation] );
 
-  render() {
-    const {
-      image,
-      error,
-      clicked,
-      numberOfHours
-    } = this.state;
+  const showMatch = useCallback( async () => {
+    if ( !seenDate && match ) {
+      await addObservation();
+      navigateToMatch();
+    } else {
+      navigateToMatch();
+    }
+  }, [navigateToMatch, seenDate, match, addObservation] );
 
-    return (
-      <>
-        <NavigationEvents
-          onWillFocus={() => this.requestAndroidPermissions()}
+  useEffect( () => {
+    if ( match !== null && clicked ) {
+      showMatch();
+    }
+  }, [match, showMatch, clicked] );
+
+  useEffect( () => {
+    navigation.addListener( "focus", () => {
+      checkMetaData();
+    } );
+  }, [navigation, checkMetaData] );
+
+  return (
+    <>
+      {error ? (
+        <ErrorScreen
+          error={error}
+          number={numberOfHours}
         />
-        {error
-          ? (
-            <ErrorScreen
-              error={error}
-              number={numberOfHours}
-            />
-          ) : (
-            <ConfirmScreen
-              updateClicked={this.checkForMatches}
-              clicked={clicked}
-              image={image.uri}
-            />
-          )}
-      </>
-    );
-  }
-}
+      ) : (
+        <ConfirmScreen
+          updateClicked={checkForMatches}
+          clicked={clicked}
+          image={image.uri}
+        />
+      )}
+    </>
+  );
+};
 
 export default OnlineServerResults;

a72f38c81258d181460ee1dfc756118a768a1f33
Amanda Bullington <albullington@gmail.com>
2020-07-24 17:58:27 -0700

    Add environment variables to Fastlane

--- components/Onboarding/Button.js
+++ components/Onboarding/Button.js
@@ -1,10 +1,6 @@
 // @flow
 import React from "react";
-import {
-  View,
-  TouchableOpacity,
-  Text
-} from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import styles from "../../styles/onboarding";

87338e0793ebd6735e0ad90f3b87f43e3d537c67
Amanda Bullington <albullington@gmail.com>
2020-09-02 16:56:04 -0700

    More library upgrades

--- components/Match/MatchModals.js
+++ components/Match/MatchModals.js
@@ -156,7 +156,6 @@ const MatchModals = ( {
   };
 
   const checkLocationPermissions = useCallback( () => {
-    console.log( image.latitude, errorCode, "checking location" );
     if ( !image.latitude ) {
       console.log( "creating location alerts", errorCode );
       createLocationAlert( errorCode );

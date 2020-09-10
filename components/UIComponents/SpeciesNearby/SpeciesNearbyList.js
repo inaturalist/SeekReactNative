42162a46102ecc3e667f34117c50ddd9de3f6bfb
Amanda Bullington <albullington@gmail.com>
2020-09-09 17:43:41 -0700

    Move renderSpecies and other functions out of FlatList rendering

--- components/UIComponents/SpeciesNearby/SpeciesNearbyList.js
+++ components/UIComponents/SpeciesNearby/SpeciesNearbyList.js
@@ -2,7 +2,6 @@ import React from "react";
 import { FlatList } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
-
 import styles from "../../../styles/uiComponents/speciesNearby/speciesNearbyList";
 import SpeciesImageCell from "./SpeciesImageCell";
 import EmptyList from "./EmptyList";
@@ -16,30 +15,38 @@ type Props = {
 const SpeciesNearbyList = ( { taxa, fetchiNatData }: Props ) => {
   const { name } = useRoute();
 
+  const getItemLayout = ( data, index ) => (
+    // skips measurement of dynamic content for faster loading
+    {
+      length: ( 28 + 108 ),
+      offset: ( 28 + 108 ) * index,
+      index
+    }
+  );
+
+  const extractKey = ( taxon, i ) => name === "ChallengeDetails" ? `observed-${i}` : `species-${taxon.id}`;
+
+  const renderEmptyList = () => <EmptyList />;
+
+  const renderSpecies = ( { item } ) => {
+    if ( name === "ChallengeDetails" ) {
+      return <SpeciesObservedCell item={item} />;
+    }
+    return <SpeciesImageCell item={item} fetchiNatData={fetchiNatData} />;
+  };
+
   return (
     <FlatList
       alwaysBounceHorizontal
       bounces={taxa.length > 0}
       contentContainerStyle={taxa.length > 0 && styles.taxonList}
       data={taxa}
-      getItemLayout={( data, index ) => (
-        // skips measurement of dynamic content for faster loading
-        {
-          length: ( 28 + 108 ),
-          offset: ( 28 + 108 ) * index,
-          index
-        }
-      )}
+      getItemLayout={getItemLayout}
       horizontal
       initialNumToRender={3}
-      keyExtractor={( taxon, i ) => name === "ChallengeDetails" ? `observed-${i}` : `species-${taxon.id}`}
-      ListEmptyComponent={() => <EmptyList />}
-      renderItem={( { item } ) => {
-      if ( name === "ChallengeDetails" ) {
-        return <SpeciesObservedCell item={item} />;
-      }
-      return <SpeciesImageCell item={item} fetchiNatData={fetchiNatData} />;
-      }}
+      keyExtractor={extractKey}
+      ListEmptyComponent={renderEmptyList}
+      renderItem={renderSpecies}
     />
   );
 };

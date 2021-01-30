645c54faba05b72d7f7ff4d673e7806ba4c4a9db
Amanda Bullington <albullington@gmail.com>
2021-01-29 18:15:26 -0800

    Outline functions for creating web token, resizing photo, and appending to observation if upload unsuccessful

--- utility/uploadHelpers.js
+++ utility/uploadHelpers.js
@@ -1,7 +1,10 @@
 // @flow
 import Realm from "realm";
+import inatjs, { FileUpload } from "inaturalistjs";
 
 import realmConfig from "../models/index";
+import createUserAgent from "../utility/userAgent";
+import { resizeImage } from "./photoHelpers";
 
 const saveIdAndUploadStatus = async ( id: number, uri: string ) => {
   const realm = await Realm.open( realmConfig );
@@ -42,6 +45,49 @@ const checkForIncompleteUploads = async ( ) => {
     console.log( Array.from( uploads.filtered( "id == 3" ) ) );
   } catch ( e ) {
     console.log( e, " : couldn't check for incomplete uploads" );
+  }
+};
+
+const resizeImageForUpload = async ( uri: string ) => {
+  return await resizeImage( uri, 2048 );
+};
+
+const fetchJSONWebToken = ( loginToken: string ) => {
+  const headers = {
+    "Content-Type": "application/json",
+    "User-Agent": createUserAgent( ),
+    "Authorization": `Bearer ${loginToken}`
+  };
+
+  const site = "https://www.inaturalist.org";
+
+  fetch( `${site}/users/api_token`, { headers } )
+    .then( r => r.json( ) )
+    .then( ( parsedResponse ) => {
+      const token = parsedResponse.api_token;
+      // next step - resize image for uploading
+    } ).catch( ( e ) => {
+      console.log( e, "couldn't create json web token" );
+  } );
+};
+
+const appendPhotoToObservation = async ( id: number, token: string, uri: string ) => {
+  const photoParams = {
+    "observation_photo[observation_id]": id,
+    file: new FileUpload( {
+      uri,
+      name: "photo.jpeg",
+      type: "image/jpeg"
+    } )
+  };
+
+  const options = { api_token: token, user_agent: createUserAgent( ) };
+
+  try {
+    await inatjs.observation_photos.create( photoParams, options );
+    return true;
+  } catch ( e ) {
+    console.log( e, "photo could not be added to image" );
   }
 };
 

68d38dd62e45d4e54894e7557d6ec6059dee90dc
Amanda Bullington <albullington@gmail.com>
2021-05-07 13:07:50 -0700

    Move business logic for ChallengeScreen into hooks folder

--- components/Challenges/ChallengeScreen.js
+++ components/Challenges/ChallengeScreen.js
@@ -1,76 +1,20 @@
 // @flow
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { View, SectionList } from "react-native";
-import Realm from "realm";
-import { useNavigation } from "@react-navigation/native";
 import type { Node } from "react";
 
-import realmConfig from "../../models";
 import styles from "../../styles/challenges/challenges";
 import ChallengeProgressCard from "./ChallengeProgressCard";
 import GreenText from "../UIComponents/GreenText";
-import { recalculateChallenges } from "../../utility/challengeHelpers";
 import NoChallenges from "../Home/Challenges/NoChallenges";
 import EmptyChallengesCard from "./EmptyChallengesCard";
 import ViewWithHeader from "../UIComponents/Screens/ViewWithHeader";
+import useFetchChallenges from "./hooks/challengeHooks";
 
-const ChallengeScreen = (): Node => {
+const ChallengeScreen = ( ): Node => {
+  const list = useFetchChallenges( );
   const sectionList = useRef( null );
-  const navigation = useNavigation();
-  const [list, setList] = useState( [] );
-
-  const fetchChallenges = () => {
-    Realm.open( realmConfig )
-      .then( ( realm ) => {
-        const challenges = realm.objects( "ChallengeRealm" ).sorted( "availableDate", true );
-        const challengesStarted = challenges.filtered( "startedDate != null AND percentComplete != 100" );
-        const challengesNotStarted = challenges.filtered( "startedDate == null" );
-        const challengesCompleted = challenges.filtered( "startedDate != null AND percentComplete == 100" );
-
-        const noChallenges = challengesNotStarted.length === 0 && challengesStarted.length === 0;
-
-        if ( noChallenges ) {
-          setList( [{
-            id: 0,
-            data: [],
-            header: "",
-            empty: ""
-          }, {
-            id: 1,
-            data: challengesCompleted,
-            header: "challenges.completed",
-            empty: "no_completed_challenges"
-          }] );
-        } else {
-          setList( [{
-            id: 0,
-            data: challengesStarted,
-            header: "challenges.in_progress",
-            empty: "no_challenges_in_progress"
-          }, {
-            id: 1,
-            data: challengesNotStarted,
-            header: "challenges.not_started",
-            empty: "no_new_challenges_header"
-          }, {
-            id: 2,
-            data: challengesCompleted,
-            header: "challenges.completed",
-            empty: "no_completed_challenges"
-          }] );
-        }
-      } ).catch( () => {
-        // console.log( "[DEBUG] Failed to open realm, error: ", err );
-      } );
-  };
-
-  useEffect( () => {
-    navigation.addListener( "focus", () => {
-      recalculateChallenges();
-      fetchChallenges();
-    } );
-  }, [navigation] );
 
   const renderSectionHeader = ( { section } ) => {
     const { id } = section;

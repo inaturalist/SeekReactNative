c2cdb3de5c5ddca1deb4a990b1269d3345419a99
Amanda Bullington <albullington@gmail.com>
2021-06-18 12:37:22 -0700

    Update version number and bitly text

--- components/Settings/Donation.js
+++ components/Settings/Donation.js
@@ -24,7 +24,7 @@ const Donation = ( { navigation }: Props ): React.Node => {
   const goBack = ( ) => navigation.goBack( );
 
   const donationPage = `${urls.DONORBOX}&utm_source=android`;
-  const bitlyForiOS = "bit.ly/3cUNTAu";
+  const bitlyForiOS = "bit.ly/3xoB9tU";
 
   return (
     <SafeAreaView style={viewStyles.container} edges={["top"]}>

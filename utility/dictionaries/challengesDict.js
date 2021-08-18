af1eaf945f471e083786aa4b7ead5f0b60d2bd44
Amanda Bullington <albullington@gmail.com>
2021-08-18 14:36:57 -0700

    Update assets; attempt to fix crash in match screen - render text

--- utility/dictionaries/challengesDict.js
+++ utility/dictionaries/challengesDict.js
@@ -321,19 +321,19 @@ const challenges = {
     ],
     availableDate: ( new Date( 2021, 7, 1 ): Date ),
     action: "seek_2021_challenges.august_2021_action"
+  },
+  sept2021: {
+    name: "seek_2021_challenges.september_2021_challenge_name",
+    description: "seek_2021_challenges.september_2021_description",
+    totalSpecies: 10,
+    backgroundName: "img-challengedetail-september2021",
+    earnedIconName: "badge_inaturalist_september2021",
+    missions: [
+      "seek_2021_challenges.september_2021_mission_1"
+    ],
+    availableDate: ( new Date( 2021, 8, 1 ): Date ),
+    action: "seek_2021_challenges.september_2021_action"
   }
-  // sept2021: {
-  //   name: "seek_2021_challenges.september_2021_challenge_name",
-  //   description: "seek_2021_challenges.september_2021_description",
-  //   totalSpecies: 10,
-  //   backgroundName: "img-challengedetail-september2021",
-  //   earnedIconName: "badge_inaturalist_september2021",
-  //   missions: [
-  //     "seek_2021_challenges.september_2021_mission_1"
-  //   ],
-  //   availableDate: ( new Date( 2021, 8, 1 ): Date ),
-  //   action: "seek_2021_challenges.september_2021_action"
-  // }
   // oct2021: {
   //   name: "seek_2021_challenges.october_2021_challenge_name",
   //   description: "seek_2021_challenges.october_2021_description",

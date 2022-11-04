1cee484acf09cb7249e3a68a3e952afceea80086
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-04 11:56:22 +0100

    Add jest setup

---
+++ tests/jest.setup.js
@@ -0,0 +1,17 @@
+import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
+import mockRNDeviceInfo from "react-native-device-info/jest/react-native-device-info-mock";
+import mockRNLocalize from "react-native-localize/mock";
+import mockRNCNetInfo from "@react-native-community/netinfo/jest/netinfo-mock";
+
+jest.mock( "@react-native-async-storage/async-storage", () => mockAsyncStorage );
+jest.mock( "react-native-device-info", () => mockRNDeviceInfo );
+jest.mock( "react-native-localize", () => mockRNLocalize );
+jest.mock( "@react-native-community/netinfo", () => mockRNCNetInfo );
+
+jest.mock( "react-native-fs", () => {
+  const RNFS = {
+    moveFile: async () => "testdata"
+  };
+
+  return RNFS;
+} );

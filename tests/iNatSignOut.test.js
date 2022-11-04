0b62bbc37d9337b0b1f358920e6b8684ff7641bf
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-04 11:58:24 +0100

    Create iNatSignOut.test.js

---
+++ tests/iNatSignOut.test.js
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react-native";
+
+import UserLoginProvider from "../components/Providers/UserLoginProvider";
+import INatSignOut from "../components/UIComponents/Login/iNatSignOut";
+
+function renderButton() {
+  return render(
+    <UserLoginProvider>
+      <INatSignOut />
+    </UserLoginProvider>
+  );
+}
+
+test( "that sign out button renders", () => {
+  renderButton( );
+} );

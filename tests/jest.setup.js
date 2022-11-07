7042be6f049c995b2d708e8b21daa197edfb2d5e
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-07 17:05:31 +0100

    Add mocks for navigation and its dependencies

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,3 +1,5 @@
+// include this line for mocking react-native-gesture-handler
+import "react-native-gesture-handler/jestSetup";
 import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
 import mockRNDeviceInfo from "react-native-device-info/jest/react-native-device-info-mock";
 import mockRNLocalize from "react-native-localize/mock";
@@ -14,4 +16,29 @@ jest.mock( "react-native-fs", () => {
   };
 
   return RNFS;
+} );
+
+// include this section and the NativeAnimatedHelper section for mocking react-native-reanimated
+jest.mock( "react-native-reanimated", () => {
+  const Reanimated = require( "react-native-reanimated/mock" );
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+} );
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock( "react-native/Libraries/Animated/NativeAnimatedHelper" );
+
+jest.mock( "@react-navigation/native", () => {
+  const actualNav = jest.requireActual( "@react-navigation/native" );
+  return {
+    ...actualNav,
+    useNavigation: () => ( {
+      navigate: jest.fn(),
+      dispatch: jest.fn()
+    } )
+  };
 } );

e83bcf3a020d4e7f32140b94d591ddb77f77d8bb
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-10 17:47:01 +0100

    Update BadgeContainer.test.js

--- tests/BadgeContainer.test.js
+++ tests/BadgeContainer.test.js
@@ -2,11 +2,11 @@ import { render } from "@testing-library/react-native";
 import BadgeContainer from "../components/Achievements/BadgeContainer";
 
 describe( "BadgeContainer", () => {
-  test( "that badge container renders", () => {
+  test( "should render correctly", () => {
     const data = [{ name: "test", earnedIconName: "test" }];
     const mockFn = jest.fn();
 
-    render( <BadgeContainer data={data} renderItem={mockFn} /> );
+    const wrapper = render( <BadgeContainer data={data} renderItem={mockFn} /> );
 
   } );
 } );

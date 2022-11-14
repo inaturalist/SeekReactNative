0a97960f17226180a7fc9908b62fe52938462dda
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-14 20:32:31 +0100

    Moved badge container test

---
+++ tests/unit/components/Achievements/BadgeContainer.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react-native";
+import BadgeContainer from "../../../../components/Achievements/BadgeContainer";
+
+describe( "BadgeContainer", () => {
+  test( "should render correctly", () => {
+    const data = [{ name: "test", earnedIconName: "test" }];
+    const mockFn = jest.fn();
+    render( <BadgeContainer data={data} renderItem={mockFn} /> );
+    expect( screen.findByTestId( "badge-container" ) ).toBeTruthy();
+    expect( mockFn ).toHaveBeenCalledTimes( 1 );
+    expect( screen ).toMatchSnapshot();
+  } );
+} );

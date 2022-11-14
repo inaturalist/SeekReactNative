68acbda8729cc57b7f526385c89dc1c5614d35d6
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-14 14:46:01 +0100

    Add two global mocks

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -4,11 +4,13 @@ import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/asy
 import mockRNDeviceInfo from "react-native-device-info/jest/react-native-device-info-mock";
 import mockRNLocalize from "react-native-localize/mock";
 import mockRNCNetInfo from "@react-native-community/netinfo/jest/netinfo-mock";
+import mockSafeAreaContext from "react-native-safe-area-context/jest/mock";
 
 jest.mock( "@react-native-async-storage/async-storage", () => mockAsyncStorage );
 jest.mock( "react-native-device-info", () => mockRNDeviceInfo );
 jest.mock( "react-native-localize", () => mockRNLocalize );
 jest.mock( "@react-native-community/netinfo", () => mockRNCNetInfo );
+jest.mock( "react-native-safe-area-context", () => mockSafeAreaContext );
 
 jest.mock( "react-native-fs", () => {
   const RNFS = {
@@ -42,3 +44,16 @@ jest.mock( "@react-navigation/native", () => {
     } )
   };
 } );
+
+jest.mock( "react-native-geolocation-service", () => ( {
+  getCurrentPosition: jest.fn().mockImplementation( ( successCallback ) => {
+    const position = {
+      coords: {
+        latitude: 42.42,
+        longitude: 42.42,
+        accuracy: 42
+      }
+    };
+    successCallback( position );
+  } )
+} ) );

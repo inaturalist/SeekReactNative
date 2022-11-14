a71b7c2dd2b1e16baab0e000288c2646871dde9b
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-14 23:55:22 +0100

    Split language picker test into two

--- tests/unit/components/Settings/LanguagePicker.test.js
+++ tests/unit/components/Settings/LanguagePicker.test.js
@@ -1,5 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react-native";
-import { Alert } from "react-native";
+import { render, screen } from "@testing-library/react-native";
 
 import { LanguageContext } from "../../../../components/UserContext";
 import LanguagePicker from "../../../../components/Settings/LanguagePicker";
@@ -22,20 +21,5 @@ describe( "LanguagePicker", ( ) => {
     expect( picker ).toBeTruthy( );
     expect( screen.findByText( "Use device language settings" ) ).toBeTruthy();
     expect( screen ).toMatchSnapshot( );
-  } );
-
-  test( "should open alert on done pressed and not before on iOS", () => {
-    const alertSpy = jest.spyOn( Alert, "alert" );
-    renderPicker();
-    const picker = screen.getByTestId( "picker" );
-
-    // Change language to "es" no alert should be shown
-    fireEvent( picker, "onValueChange", "es" );
-    expect( alertSpy ).not.toHaveBeenCalled( );
-    // Press done, show confirmation alert
-    fireEvent( picker, "onDonePress" );
-    expect( alertSpy ).toHaveBeenCalled();
-    // TODO: this works with the iOS picker, but not with the Android one
-    // because Platform.OS always returns "ios" in the test environment
   } );
 } );

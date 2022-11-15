d1fe32fc78131020a6dab1100eb362d4f7dae9cb
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-15 13:56:59 +0100

    Moved sign out button unit test

---
+++ tests/unit/components/UIComponents/Login/iNatSignOut.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react-native";
+
+import UserLoginProvider from "../../../../../components/Providers/UserLoginProvider";
+import INatSignOut from "../../../../../components/UIComponents/Login/iNatSignOut";
+
+function renderButton( user ) {
+  return render(
+    <UserLoginProvider value={user}>
+      <INatSignOut />
+    </UserLoginProvider>
+  );
+}
+
+describe( "iNatSignOut", () => {
+  test( "should render correctly", () => {
+    renderButton();
+    screen.findByText( "SIGN OUT OF INATURALIST" );
+    expect( screen ).toMatchSnapshot();
+  } );
+} );

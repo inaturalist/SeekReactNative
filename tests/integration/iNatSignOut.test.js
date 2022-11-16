39ec7c08ebe6d06f5620d5cb94b7aa64fd80835c
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-16 10:21:59 +0100

    Started SignOut button integration test

---
+++ tests/integration/iNatSignOut.test.js
@@ -0,0 +1,30 @@
+import { render, screen, fireEvent } from "@testing-library/react-native";
+
+import UserLoginProvider from "../../components/Providers/UserLoginProvider";
+import INatSignOut from "../../components/UIComponents/Login/iNatSignOut";
+
+function renderButton( user ) {
+  return render(
+    <UserLoginProvider value={user}>
+      <INatSignOut />
+    </UserLoginProvider>
+);
+}
+
+describe( "iNatSignOut", () => {
+  test( "press signs the user out", () => {
+    const user = {
+      login: "some_token",
+      userProfile: {
+        login: "some_name",
+        icon: "some_photo"
+      }
+    };
+
+    renderButton( user );
+    const button = screen.getByText( "SIGN OUT OF INATURALIST" );
+    fireEvent.press( button );
+    // TODO: test that the user is signed out
+
+  } );
+} );

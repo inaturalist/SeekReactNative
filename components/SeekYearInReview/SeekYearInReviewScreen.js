a8f744fbe88d0da12e628be6e32c946b4001357c
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-16 17:37:04 +0100

    Add some styles, data logic, elements and many TODOs

--- components/SeekYearInReview/SeekYearInReviewScreen.js
+++ components/SeekYearInReview/SeekYearInReviewScreen.js
@@ -1,24 +1,91 @@
 // @flow
 
 import React, { useContext } from "react";
-import { View } from "react-native";
+import { View, Text, Image } from "react-native";
 import type { Node } from "react";
 
-import { viewStyles } from "../../styles/seekYearInReview/seekYearInReview";
-import { AppOrientationContext } from "../UserContext";
+import {
+  viewStyles,
+  textStyles,
+  imageStyles
+} from "../../styles/seekYearInReview/seekYearInReview";
+import { AppOrientationContext, UserContext } from "../UserContext";
 import ScrollWithHeader from "../UIComponents/Screens/ScrollWithHeader";
+import { useFetchObservationCount, useFetchStats } from "./hooks/seekYearInReviewHooks";
+import badgeImages from "../../assets/badges";
+// TODO: refactor into component folder
+import SpeciesBadges from "../Achievements/SpeciesBadges";
+import HorizontalScroll from "../UIComponents/HorizontalScroll";
 
 const SeekYearInReviewScreen = (): Node => {
   const { isTablet } = useContext( AppOrientationContext );
+  const { userProfile, login } = useContext( UserContext );
+  const count = useFetchObservationCount( login, userProfile.login );
+  // TODO: replace with real year
+  const state = useFetchStats( 2022 );
+
+  const renderPhotos = () =>
+    state.randomObservations.map( ( obs ) => (
+      <View
+        key={`image${obs.taxon.defaultPhoto.mediumUrl}`}
+        style={viewStyles.center}
+      >
+        <Image
+          source={{ uri: obs.taxon.defaultPhoto.mediumUrl }}
+          style={imageStyles.image}
+        />
+        {console.log( obs.taxon.defaultPhoto.mediumUrl ) ? null : null}
+      </View>
+    ) );
+
+  const photoList = renderPhotos();
 
   return (
-    <ScrollWithHeader header="seek_year_in_review.header" footer>
+    <ScrollWithHeader header="seek_year_in_review.header" footer={false}>
       <View
         style={[
           viewStyles.textContainer,
           isTablet && viewStyles.tabletContainer
         ]}
       />
+      {/* TODO: replace all instances of text with StyledText after v2.14.5 is merged */}
+      {/* {state.countObservationsThisYear > 0 && ( */}
+      {
+        <Text style={textStyles.text}>
+          {state.countObservationsThisYear} observations made this year
+        </Text>
+      }
+      {login && (
+        <Text style={textStyles.text}>
+          {count} observations posted to iNaturalist this year
+        </Text>
+      )}
+      {state.level && (
+        <Image
+          source={badgeImages[state.level.earnedIconName]}
+          style={imageStyles.levelImage}
+        />
+      )}
+      {state.topThreeIconicTaxonIds && (
+        <Text style={textStyles.text}>
+          [{state.topThreeIconicTaxonIds}] top three taxa IDs
+        </Text>
+      )}
+      <SpeciesBadges speciesBadges={state.topThreeSpeciesBadges} />
+      {state.countBadgesThisYear && (
+        <Text style={textStyles.text}>
+          {state.countBadgesThisYear} badges earned this year
+        </Text>
+      )}
+      {state.randomObservations &&
+        state.randomObservations.map( ( obs ) => (
+          <Text style={textStyles.text}>
+            {obs.uuidString} is a random observation
+          </Text>
+        ) )}
+      <View style={viewStyles.photoMargins}>
+        <HorizontalScroll photoList={photoList} />
+      </View>
     </ScrollWithHeader>
   );
 };

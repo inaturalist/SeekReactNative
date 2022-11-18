f9431d2fd09a188e94e635f641451f4efa94e9a4
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-18 10:49:36 +0100

    Combine language picker tests

--- tests/unit/components/Settings/LanguagePicker.test.js
+++ tests/unit/components/Settings/LanguagePicker.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, screen } from "@testing-library/react-native";
+import { render, screen, fireEvent } from "@testing-library/react-native";
+import { Alert } from "react-native";
 
 import { LanguageContext } from "../../../../components/UserContext";
 import LanguagePicker from "../../../../components/Settings/LanguagePicker";
@@ -25,4 +26,37 @@ describe( "LanguagePicker", () => {
     expect( screen.findByText( "Use device language settings" ) ).toBeTruthy();
     expect( screen ).toMatchSnapshot();
   } );
+
+    test( "should open alert on done pressed and not before on iOS", () => {
+      const alertSpy = jest.spyOn( Alert, "alert" );
+      renderPicker();
+      const picker = screen.getByTestId( "picker" );
+
+      // Change language to "es" no alert should be shown
+      fireEvent( picker, "onValueChange", "es" );
+      expect( alertSpy ).not.toHaveBeenCalled();
+      // Press done, show confirmation alert
+      fireEvent( picker, "onDonePress" );
+      expect( alertSpy ).toHaveBeenCalled();
+      // TODO: this works with the iOS picker, but not with the Android one
+      // because Platform.OS always returns "ios" in the test environment
+    } );
+
+    test( "should change the language to Spanish", () => {
+      renderPicker();
+      const picker = screen.getByTestId( "picker" );
+
+      // Change language to "es" no alert should be shown
+      fireEvent( picker, "onValueChange", "es" );
+      fireEvent( picker, "onDonePress" );
+
+      // TODO: Press confirm on the Alert, could not get this to work
+      // const alertSpy = jest.spyOn( Alert, "alert" );
+      // alertSpy.mock.calls[0][2][0].onPress();
+
+      // TODO: Text should be in Spanish
+      // expect( screen.findByText( "Usar las opciones de idioma del móvil" ) ).toBeTruthy();
+      // expect( screen ).toMatchSnapshot();
+    } );
+
 } );

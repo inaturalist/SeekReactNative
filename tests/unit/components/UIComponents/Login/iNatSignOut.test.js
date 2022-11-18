176d9928106d06dd6471bd1062329d5f163381a4
Johannes Klein <johannes.t.klein@gmail.com>
2022-11-18 10:46:18 +0100

    Combining two tests

--- tests/unit/components/UIComponents/Login/iNatSignOut.test.js
+++ tests/unit/components/UIComponents/Login/iNatSignOut.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react-native";
+import { render, screen, fireEvent } from "@testing-library/react-native";
 
 import UserLoginProvider from "../../../../../components/Providers/UserLoginProvider";
 import INatSignOut from "../../../../../components/UIComponents/Login/iNatSignOut";
@@ -17,5 +17,20 @@ describe( "iNatSignOut", () => {
     renderButton();
     screen.findByText( "SIGN OUT OF INATURALIST" );
     expect( screen ).toMatchSnapshot();
+  } );
+
+  test( "press signs the user out", () => {
+    const user = {
+      login: "some_token",
+      userProfile: {
+        login: "some_name",
+        icon: "some_photo"
+      }
+    };
+
+    renderButton( user );
+    const button = screen.getByText( "SIGN OUT OF INATURALIST" );
+    fireEvent.press( button );
+    // TODO: test that the user is signed out
   } );
 } );

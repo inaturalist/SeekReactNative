12cadaf074c0441b43d95daa02d61be90c2769fb
Johannes Klein <johannes.t.klein@gmail.com>
2022-12-02 14:17:54 +0100

    Add babel plugin module resolver

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,15 @@ module.exports = ( api ) => {
   ];
   const plugins = [
     "@babel/plugin-transform-flow-strip-types",
-    "react-native-reanimated/plugin" // Reanimated 2 plugin has to be listed last
+    [
+      "module-resolver",
+      {
+        alias: {
+          tests: "./tests"
+        }
+      }
+    ],
+    "react-native-reanimated/plugin" // Reanimated 2 plugin has to be listed last https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/
   ];
   const productionPlugins = ["transform-remove-console"].concat( plugins );
 

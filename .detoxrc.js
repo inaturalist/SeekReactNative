a714c18af1a1587ed590da2c2a091a781b0aaead
Johannes Klein <johannes.t.klein@gmail.com>
2022-12-05 14:40:24 +0100

    Add detox, and first e2e test

---
+++ .detoxrc.js
@@ -0,0 +1,42 @@
+/** @type {Detox.DetoxConfig} */
+module.exports = {
+  testRunner: {
+    args: {
+      '$0': 'jest',
+      config: 'e2e/jest.config.js'
+    },
+    jest: {
+      setupTimeout: 120000
+    }
+  },
+  apps: {
+    'ios.debug': {
+      type: 'ios.app',
+      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/Seek.app',
+      build: 'xcodebuild -workspace ios/Seek.xcworkspace -scheme Seek -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+    },
+    'ios.release': {
+      type: 'ios.app',
+      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/Seek.app',
+      build: 'xcodebuild -workspace ios/Seek.xcworkspace -scheme Seek -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+    }
+  },
+  devices: {
+    simulator: {
+      type: 'ios.simulator',
+      device: {
+        type: 'iPhone 13'
+      }
+    }
+  },
+  configurations: {
+    'ios.sim.debug': {
+      device: 'simulator',
+      app: 'ios.debug'
+    },
+    'ios.sim.release': {
+      device: 'simulator',
+      app: 'ios.release'
+    }
+  }
+};
---
+++ e2e/first.test.js
@@ -0,0 +1,20 @@
+import { device, element, by, waitFor } from "detox";
+
+describe( "First app start test", () => {
+  beforeAll( async () => {
+    await device.launchApp( { newInstance: true, permissions: { location: "always" }, languageAndLocale: { language: "en", locale: "US" } } );
+  } );
+
+  beforeEach( async () => {
+    await device.reloadReactNative();
+  } );
+
+  it( "should land on home screen", async () => {
+    // Await the loading of the home screen
+    await device.takeScreenshot( "first open" );
+    await waitFor( element( by.text( "GET STARTED" ) ) ).toBeVisible().withTimeout( 4000 );
+    await waitFor( element( by.text( "CONTINUE" ) ) ).toBeVisible();
+    await element( by.text( "CONTINUE" ) ).tap();
+    await expect( element( by.text( "SPECIES NEARBY" ) ) ).toBeVisible();
+  } );
+} );

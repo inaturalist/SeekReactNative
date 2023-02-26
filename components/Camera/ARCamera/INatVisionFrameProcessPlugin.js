66dc8b7e53a14dfefc0d4459c32be9c75ff723e7
Johannes Klein <johannes.t.klein@gmail.com>
2023-02-26 12:21:17 +0100

    Delete INatVisionFrameProcessPlugin.js

--- components/Camera/ARCamera/INatVisionFrameProcessPlugin.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-export function inatVision( frame, modelFilename, taxonomyFilename ) {
-  "worklet";
-  // __inatVision is defined in global in babel.config.js
-  // eslint-disable-next-line no-undef
-  return __inatVision( frame, modelFilename, taxonomyFilename );
-}

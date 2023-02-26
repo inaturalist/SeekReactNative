dc6cc2a9d14ba5017f8696bd3c8e65b3fa767fe5
Johannes Klein <johannes.t.klein@gmail.com>
2023-02-26 12:45:01 +0100

    Update MainApplication.java

--- android/app/src/main/java/org/inaturalist/seek/MainApplication.java
+++ android/app/src/main/java/org/inaturalist/seek/MainApplication.java
@@ -11,7 +11,6 @@ import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.cameraroll.CameraRollPackage;
-import org.inaturalist.inatcamera.nativecamera.INatCameraViewPackage;
 
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -21,6 +20,8 @@ import com.facebook.soloader.SoLoader;
 import org.inaturalist.seek.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import org.inaturalist.inatcamera.nativecamera.INatCameraViewPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -36,7 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-        packages.add( new INatCameraViewPackage() );
+      packages.add( new INatCameraViewPackage() );
       return packages;
     }
 

4bda87ab23f42265ca6c544b5d78beaf42e0bfaf
Johannes Klein <johannes.t.klein@gmail.com>
2023-03-17 14:18:03 +0100

    Enable tap to focus

--- components/Camera/ARCamera/FrameProcessorCamera.js
+++ components/Camera/ARCamera/FrameProcessorCamera.js
@@ -8,7 +8,7 @@ import {
     useFrameProcessor
 } from "react-native-vision-camera";
 import * as REA from "react-native-reanimated";
-
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import { inatVision } from "vision-camera-plugin-inatvision";
 
 import type { Node } from "react";
@@ -16,6 +16,30 @@ import type { Node } from "react";
 const FrameProcessorCamera = ( props ): Node => {
   const devices = useCameraDevices();
   const device = devices.back;
+
+  const singleTapToFocus = async ( { x, y } ) => {
+    try {
+      await props.cameraRef.current.focus( { x, y } );
+    } catch ( e ) {
+      // Android often catches the following error from the Camera X library
+      // but it doesn't seem to affect functionality, so we're ignoring this error
+      // and throwing other errors
+      const startFocusError = e?.message?.includes(
+        "Cancelled by another startFocusAndMetering"
+      );
+      if ( !startFocusError ) {
+        throw e;
+      }
+    }
+  };
+
+  const singleTap = Gesture.Tap()
+    .runOnJS( true )
+    .maxDuration( 250 )
+    .numberOfTaps( 1 )
+    .onStart( ( e ) => {
+      singleTapToFocus( e );
+    } );
 
   const frameProcessor = useFrameProcessor( ( frame ) => {
     "worklet";
@@ -41,6 +65,18 @@ const FrameProcessorCamera = ( props ): Node => {
         frameProcessor={frameProcessor}
         frameProcessorFps={1}
       />
+        <GestureDetector gesture={Gesture.Exclusive( singleTap )}>
+          <Camera
+            ref={props.cameraRef}
+            style={styles.camera}
+            enableZoomGesture
+            photo={true}
+            device={device}
+            isActive={true}
+            frameProcessor={frameProcessor}
+            frameProcessorFps={1}
+          />
+        </GestureDetector>
     )
   );
 };

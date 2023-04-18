f7ebc5a38e9ab1ef7df0f42a1c9868425dc456ce
Johannes Klein <johannes.t.klein@gmail.com>
2023-04-18 15:52:12 +0200

    Upgrade to RN 0.71.6

--- android/app/src/main/java/org/inaturalist/seek/MainApplication.java
+++ android/app/src/main/java/org/inaturalist/seek/MainApplication.java
@@ -1,31 +1,23 @@
 package org.inaturalist.seek;
 
 import android.app.Application;
-import android.content.Context;
-// SharedPreferences and File are for the Google Maps fix
-import android.content.SharedPreferences;
-import java.io.File;
 
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.cameraroll.CameraRollPackage;
-import org.inaturalist.inatcamera.nativecamera.INatCameraViewPackage;
-
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-import org.inaturalist.seek.newarchitecture.MainApplicationReactNativeHost;
-import java.lang.reflect.InvocationTargetException;
+
+import org.inaturalist.inatcamera.nativecamera.INatCameraViewPackage;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+      new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -36,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-        packages.add( new INatCameraViewPackage() );
+      packages.add( new INatCameraViewPackage() );
       return packages;
     }
 
@@ -44,77 +36,30 @@ public class MainApplication extends Application implements ReactApplication {
     protected String getJSMainModuleName() {
       return "index";
     }
-  };
 
-  private final ReactNativeHost mNewArchitectureNativeHost = new MainApplicationReactNativeHost(this);
+        @Override
+        protected boolean isNewArchEnabled() {
+          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+        @Override
+        protected Boolean isHermesEnabled() {
+          return BuildConfig.IS_HERMES_ENABLED;
+        }
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      return mNewArchitectureNativeHost;
-    } else {
-      return mReactNativeHost;
-    }
+    return mReactNativeHost;
   }
 
   @Override
   public void onCreate() {
-    super.onCreate();
-    // try {
-    //   /*
-    //     This try/catch is intended to patch fix the Google Maps server issue introduced 4/23/20
-    //   */
-    //   SharedPreferences hasFixedGoogleBug154855417 = getSharedPreferences("google_bug_154855417", Context.MODE_PRIVATE);
-    //   if (!hasFixedGoogleBug154855417.contains("fixed")) {
-    //     File corruptedZoomTables = new File(getFilesDir(), "ZoomTables.data");
-    //     File corruptedSavedClientParameters = new File(getFilesDir(), "SavedClientParameters.data.cs");
-    //     File corruptedClientParametersData =
-    //         new File(
-    //           getFilesDir(),
-    //           "DATA_ServerControlledParametersManager.data.v1."
-    //               + getBaseContext().getPackageName());
-    //     corruptedZoomTables.delete();
-    //     corruptedSavedClientParameters.delete();
-    //     corruptedClientParametersData.delete();
-    //     hasFixedGoogleBug154855417.edit().putBoolean("fixed", true).apply();
-    //   }
-    // } catch (Exception e) {
-
-    // }
-    
-    // If you opted-in for the New Architecture, we enable the TurboModule system
-    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    super.onCreate();    
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager()); // Remove this line if you don't want Flipper enabled
-  }
-
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
-   */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("org.inaturalist.seek.ReactNativeFlipper");
-        aClass.getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-          .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      DefaultNewArchitectureEntryPoint.load();
     }
+    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
--- ios/Seek/AppDelegate.mm
+++ ios/Seek/AppDelegate.mm
@@ -1,99 +1,17 @@
 #import "AppDelegate.h"
 
-#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
-
-#import <React/RCTAppSetupUtils.h>
-
-#import <RNQuickActionManager.h>
-
-static NSString *hasMigratedRealmDatabaseFromContainer = @"HasMigratedRealmDatabaseFromContainer";
-static NSString *hasMigratedPhotosFromContainer = @"HasMigratedPhotosFromContainer";
-static NSString *appGroupId = @"group.org.inaturalist.CardsSharing";
-
-#if RCT_NEW_ARCH_ENABLED
-#import <React/CoreModulesPlugins.h>
-#import <React/RCTCxxBridgeDelegate.h>
-#import <React/RCTFabricSurfaceHostingProxyRootView.h>
-#import <React/RCTSurfacePresenter.h>
-#import <React/RCTSurfacePresenterBridgeAdapter.h>
-#import <ReactCommon/RCTTurboModuleManager.h>
-
-#import <react/config/ReactNativeConfig.h>
-
-static NSString *const kRNConcurrentRoot = @"concurrentRoot";
-
-@interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
-  RCTTurboModuleManager *_turboModuleManager;
-  RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
-  std::shared_ptr<const facebook::react::ReactNativeConfig> _reactNativeConfig;
-  facebook::react::ContextContainer::Shared _contextContainer;
-}
-@end
-#endif
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTAppSetupPrepareApp(application);
+  self.moduleName = @"Seek";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
 
-  if (![[NSUserDefaults standardUserDefaults] boolForKey:hasMigratedRealmDatabaseFromContainer]) {
-    [self migrateRealmDatabaseFromSharedContainer];
-  }
-
-  if (![[NSUserDefaults standardUserDefaults] boolForKey:hasMigratedPhotosFromContainer]) {
-    [self migratePhotosFromSharedContainer];
-  }
-
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-
-#if RCT_NEW_ARCH_ENABLED
-  _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
-  _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
-  _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
-  _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
-  bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
-#endif
-
-  NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Seek", initProps);
-
-  if (@available(iOS 13.0, *)) {
-    rootView.backgroundColor = [UIColor systemBackgroundColor];
-  } else {
-    rootView.backgroundColor = [UIColor whiteColor];
-  }
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return YES;
-}
-
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feture is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-  // Switch this bool to turn on and off the concurrent root
-  return true;
-}
-
-- (NSDictionary *)prepareInitialProps
-{
-  NSMutableDictionary *initProps = [NSMutableDictionary new];
-
-#ifdef RCT_NEW_ARCH_ENABLED
-  initProps[kRNConcurrentRoot] = @([self concurrentRootEnabled]);
-#endif
-
-  return initProps;
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
@@ -105,132 +23,14 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 }
 
-- (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL succeeded)) completionHandler {
-  [RNQuickActionManager onQuickActionPress:shortcutItem completionHandler:completionHandler];
-}
-- (void)migrateRealmDatabaseFromSharedContainer {
-  NSString *realmDbFilename = @"db.realm";
-  // fm lets us do operations on the filesystem
-  NSFileManager *fm = [NSFileManager defaultManager];
-  // base url for the shared container
-  NSURL *containerUrl = [fm containerURLForSecurityApplicationGroupIdentifier:appGroupId];
-  if (containerUrl) {
-    // url for the realm database in the container
-    NSURL *containerRealmUrl = [containerUrl URLByAppendingPathComponent:realmDbFilename];
-    if ([fm fileExistsAtPath:containerRealmUrl.path]) {
-      // get the app document directory
-      NSURL *documentsUrl =   [[fm URLsForDirectory:NSDocumentDirectory
-                                          inDomains:NSUserDomainMask] lastObject];
-      // url for the realm database in the app documents directory
-      NSURL *documentsRealmUrl = [documentsUrl URLByAppendingPathComponent:realmDbFilename];
-      // move error will go here
-      NSError *moveError = nil;
-      // perform the move
-      [fm moveItemAtURL:containerRealmUrl toURL:documentsRealmUrl error:&moveError];
-      if (moveError) {
-        NSLog(@"error moving file: %@", moveError.localizedDescription);
-      }
-    }
-  }
-
-  // update the user defaults
-  [[NSUserDefaults standardUserDefaults] setBool:YES
-                                          forKey:hasMigratedRealmDatabaseFromContainer];
-  [[NSUserDefaults standardUserDefaults] synchronize];
-}
-
-- (void)migratePhotosFromSharedContainer {
-  // fm lets us do operations on the filesystem
-  NSFileManager *fm = [NSFileManager defaultManager];
-  // base url for the shared container
-  NSURL *containerUrl = [fm containerURLForSecurityApplicationGroupIdentifier:appGroupId];
-  if (containerUrl) {
-    // url for the photo directory in the container
-    NSURL *containerPhotoBaseUrl = [containerUrl URLByAppendingPathComponent:@"large"];
-    NSString *containerPhotoBasePath = [containerPhotoBaseUrl path];
-    
-    // documents directory for photos
-    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    NSString *documentPhotoBasePath = [documentsPath stringByAppendingPathComponent:@"large"];
-    if (![fm fileExistsAtPath:documentPhotoBasePath]) {
-      NSError *mkdirError = nil;
-      [fm createDirectoryAtPath:documentPhotoBasePath
-    withIntermediateDirectories:NO
-                     attributes:nil
-                          error:&mkdirError];
-      if (mkdirError) {
-        NSLog(@"error creating large directory: %@", mkdirError.localizedDescription);
-        return;
-      }
-    }
-    
-    NSError *fileScanError = nil;
-    NSArray *contents = [fm contentsOfDirectoryAtPath:containerPhotoBasePath
-                                                error:&fileScanError];
-    if (fileScanError) {
-      NSLog(@"error scanning filesystem: %@", fileScanError.localizedDescription);
-      return;
-    }
-    
-    for (NSString *containerPhoto in contents) {
-      NSString *containerPhotoPath = [containerPhotoBasePath stringByAppendingPathComponent:containerPhoto];
-      NSString *documentPhotoPath = [documentPhotoBasePath stringByAppendingPathComponent:containerPhoto];
-      
-      NSError *moveError = nil;
-      [fm moveItemAtPath:containerPhotoPath
-                  toPath:documentPhotoPath
-                   error:&moveError];
-      if (moveError) {
-        NSLog(@"error moving photo file: %@", moveError.localizedDescription);
-        return;
-      }
-    }
-    
-    // update the user defaults
-    [[NSUserDefaults standardUserDefaults] setBool:YES
-                                            forKey:hasMigratedPhotosFromContainer];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-
-  }
-}
-
-#if RCT_NEW_ARCH_ENABLED
-
-#pragma mark - RCTCxxBridgeDelegate
-
-- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
 {
-  _turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
-                                                             delegate:self
-                                                            jsInvoker:bridge.jsCallInvoker];
-  return RCTAppSetupDefaultJsExecutorFactory(bridge, _turboModuleManager);
+  return true;
 }
-
-#pragma mark RCTTurboModuleManagerDelegate
-
-- (Class)getModuleClassFromName:(const char *)name
-{
-  return RCTCoreModulesClassProvider(name);
-}
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
-                                                      jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
-{
-  return nullptr;
-}
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
-                                                     initParams:
-                                                         (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-  return nullptr;
-}
-
-- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
-{
-  return RCTAppSetupDefaultModuleFromClass(moduleClass);
-}
-
-#endif
 
 @end

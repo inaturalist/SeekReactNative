42b6379b83135d0cbd8d7aa38dc2f123ab04a5a3
Johannes Klein <johannes.t.klein@gmail.com>
2023-04-18 21:50:50 +0200

    Rename new android model files

--- constants/modelFileNames.js
+++ constants/modelFileNames.js
@@ -1,6 +1,6 @@
 export default {
   IOSMODEL: "optimized_model.mlmodelc",
   IOSTAXONOMY: "taxonomy.json",
-  ANDROIDMODEL: "optimized_model.tflite",
-  ANDROIDTAXONOMY: "taxonomy.csv"
+  ANDROIDMODEL: "optimized_model_2_1.tflite",
+  ANDROIDTAXONOMY: "taxonomy_2_1.csv"
 };

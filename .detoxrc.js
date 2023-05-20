6aed813b786a0596e3772a49ec76a0ec269fb69f
Johannes Klein <johannes.t.klein@gmail.com>
2023-05-20 21:11:46 +0200

    Metro bundler flag to include e2e-mock files for e2e tests

--- .detoxrc.js
+++ .detoxrc.js
@@ -2,41 +2,43 @@
 module.exports = {
   testRunner: {
     args: {
-      '$0': 'jest',
-      config: 'e2e/jest.config.js'
+      $0: "jest",
+      config: "e2e/jest.config.js",
     },
     jest: {
-      setupTimeout: 120000
-    }
+      setupTimeout: 120000,
+    },
   },
   apps: {
-    'ios.debug': {
-      type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/Seek.app',
-      build: 'xcodebuild -workspace ios/Seek.xcworkspace -scheme Seek -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+    "ios.debug": {
+      type: "ios.app",
+      binaryPath: "ios/build/Build/Products/Debug-iphonesimulator/Seek.app",
+      build:
+        "export MOCK_MODE=e2e && xcodebuild -workspace ios/Seek.xcworkspace -scheme Seek -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build && unset MOCK_MODE",
     },
-    'ios.release': {
-      type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/Seek.app',
-      build: 'xcodebuild -workspace ios/Seek.xcworkspace -scheme Seek -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
-    }
+    "ios.release": {
+      type: "ios.app",
+      binaryPath: "ios/build/Build/Products/Release-iphonesimulator/Seek.app",
+      build:
+        "export MOCK_MODE=e2e && xcodebuild -workspace ios/Seek.xcworkspace -scheme Seek -configuration Release -sdk iphonesimulator -derivedDataPath ios/build && unset MOCK_MODE",
+    },
   },
   devices: {
     simulator: {
-      type: 'ios.simulator',
+      type: "ios.simulator",
       device: {
-        type: 'iPhone 14 Plus'
-      }
-    }
+        type: "iPhone 14 Plus",
+      },
+    },
   },
   configurations: {
-    'ios.sim.debug': {
-      device: 'simulator',
-      app: 'ios.debug'
+    "ios.sim.debug": {
+      device: "simulator",
+      app: "ios.debug",
     },
-    'ios.sim.release': {
-      device: 'simulator',
-      app: 'ios.release'
-    }
-  }
+    "ios.sim.release": {
+      device: "simulator",
+      app: "ios.release",
+    },
+  },
 };

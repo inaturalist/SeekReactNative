93a62b32a150dcfa79db3b94bb6c2dcf5c5adbc5
Johannes Klein <johannes.t.klein@gmail.com>
2023-05-20 21:09:40 +0200

    Camera e2e navigate to camera

---
+++ e2e/camera.test.js
@@ -0,0 +1,32 @@
+import { device, element, by, waitFor } from "detox";
+
+describe( "First app start test", () => {
+  beforeAll( async () => {
+    await device.launchApp( {
+      newInstance: true,
+      permissions: { location: "always", camera: "YES", microphone: "YES", medialibrary: "YES", photos: "YES" },
+      languageAndLocale: { language: "en", locale: "US" }
+    } );
+  } );
+
+  beforeEach( async () => {
+    await device.reloadReactNative();
+  } );
+
+  it( "should navigate to camera screen", async () => {
+    // Await the loading of the home screen
+    await waitFor( element( by.text( "GET STARTED" ) ) )
+      .toBeVisible()
+      .withTimeout( 4000 );
+    await waitFor( element( by.text( "CONTINUE" ) ) ).toBeVisible();
+    await element( by.text( "CONTINUE" ) ).tap();
+    await expect( element( by.text( "SPECIES NEARBY" ) ) ).toBeVisible();
+    await element( by.id( "openCameraButton" ) ).tap();
+    await expect( element( by.text( "REMEMBER" ) ) ).toBeVisible();
+    await waitFor( element( by.text( "CONTINUE" ) ) ).toBeVisible();
+    await element( by.text( "CONTINUE" ) ).tap();
+    await element( by.id( "takePhotoButton" ) ).tap();
+    // Mock how to take an actual photo
+
+  } );
+} );

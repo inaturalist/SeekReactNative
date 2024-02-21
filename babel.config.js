2efb9e834a0a2424d673c7534851160fc6c19d4a
Johannes Klein <johannes.t.klein@gmail.com>
2024-02-21 17:13:09 +0100

    Include processNestedWorklets flag for babel

--- babel.config.js
+++ babel.config.js
@@ -14,8 +14,14 @@ module.exports = ( api ) => {
         }
       }
     ],
-    "react-native-reanimated/plugin"
     // Reanimated 2 plugin has to be listed last https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/
+    // processNestedWorklets is required for vision-camera together with reanimated to work
+    [
+      "react-native-reanimated/plugin",
+      {
+        processNestedWorklets: true
+      }
+    ]
   ];
   const productionPlugins = ["transform-remove-console"].concat( plugins );
 

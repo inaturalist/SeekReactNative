1e32b54e0f092bad47092b05226a43f1a956eca1
Johannes Klein <johannes.t.klein@gmail.com>
2024-05-07 12:00:34 +0200

    LocationPickerButton TS

--- components/Home/SpeciesNearby/LocationPickerButton.tsx
+++ components/Home/SpeciesNearby/LocationPickerButton.tsx
@@ -1,31 +1,27 @@
-// @flow
-
 import React from "react";
 import {
   View,
   TouchableOpacity,
   Image
 } from "react-native";
-import type { Node } from "react";
 
-import { colors } from "../../../styles/global";
 import { viewStyles, imageStyles, textStyles } from "../../../styles/home/speciesNearby";
 import { baseTextStyles } from "../../../styles/textStyles";
 import posting from "../../../assets/posting";
 import i18n from "../../../i18n";
 import StyledText from "../../UIComponents/StyledText";
 
-type Props = {
-  openLocationPicker: ( ) => void,
-  disabled: boolean,
-  location: string
+interface Props {
+  readonly openLocationPicker: ( ) => void;
+  readonly disabled: boolean;
+  readonly location: string;
 }
 
 const LocationPickerButton = ( {
   openLocationPicker,
   disabled,
   location
-}: Props ): Node => {
+}: Props ) => {
   const locationText = location ? location : i18n.t( "species_nearby.no_location" );
   return (
     <TouchableOpacity
@@ -33,8 +29,7 @@ const LocationPickerButton = ( {
       style={[viewStyles.row, viewStyles.locationPickerButton]}
       disabled={disabled}
     >
-      {/* $FlowFixMe */}
-      <Image source={posting.location} tintColor={colors.white} style={imageStyles.image} />
+      <Image source={posting.location} style={imageStyles.image} />
       <View style={viewStyles.whiteButton}>
         <StyledText style={[baseTextStyles.buttonGreen, textStyles.buttonText]}>
           {locationText.toLocaleUpperCase( )}

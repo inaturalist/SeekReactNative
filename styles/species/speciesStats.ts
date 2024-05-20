937c9071cdc712a12988592d5612d60d8357adb7
Johannes Klein <johannes.t.klein@gmail.com>
2024-05-20 22:44:23 +0200

    Remove padding iOS from species detail tags rectangle

--- styles/species/speciesStats.ts
+++ styles/species/speciesStats.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { colors, padding } from "../global";
+import { colors } from "../global";
 
 const spaceBelowTags = 6;
 
@@ -28,8 +28,7 @@ const viewStyles = StyleSheet.create( {
 
 const textStyles = StyleSheet.create( {
   tagText: {
-    paddingHorizontal: 9,
-    paddingTop: padding.iOSButtonPadding
+    paddingHorizontal: 9
   }
 } );
 

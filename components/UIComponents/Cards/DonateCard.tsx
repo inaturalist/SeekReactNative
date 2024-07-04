966573dcb639cda3e4615bc3cff8cff1bda17c69
Johannes Klein <johannes.t.klein@gmail.com>
2024-07-04 11:50:42 +0200

    Remove link to donate screen

--- components/UIComponents/Cards/DonateCard.tsx
+++ components/UIComponents/Cards/DonateCard.tsx
@@ -1,23 +1,23 @@
 import * as React from "react";
 import { View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+// import { useNavigation } from "@react-navigation/native";
 import { useRoute } from "@react-navigation/native";
 
 import i18n from "../../../i18n";
 import { viewStyles, textStyles } from "../../../styles/uiComponents/cards/donateCard";
-import GreenButton from "../Buttons/GreenButton";
+// import GreenButton from "../Buttons/GreenButton";
 import StyledText from "../StyledText";
 import { baseTextStyles } from "../../../styles/textStyles";
 import { useAppOrientation } from "../../Providers/AppOrientationProvider";
 
 const DonateCard = ( ) => {
-  const { navigate } = useNavigation( );
+  // const { navigate } = useNavigation( );
   const { name } = useRoute( );
   const { isLandscape } = useAppOrientation( );
 
   const isHomeScreen = name === "Home";
 
-  const navToDonation = ( ) => navigate( "Donation" );
+  // const navToDonation = ( ) => navigate( "Donation" );
 
   return (
     <View style={viewStyles.whiteContainer}>
@@ -42,11 +42,11 @@ const DonateCard = ( ) => {
         viewStyles.marginGreenButton,
         isHomeScreen && viewStyles.marginGreenButtonLarge
       ]} />
-      <GreenButton
+      {/* <GreenButton
         text="settings.donate"
         handlePress={navToDonation}
       />
-      <View style={viewStyles.marginBottom} />
+      <View style={viewStyles.marginBottom} /> */}
     </View>
   );
 };

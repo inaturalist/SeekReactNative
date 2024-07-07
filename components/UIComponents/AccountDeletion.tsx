9a3a649a29add489f8db9ecc66f5686a11943f67
Johannes Klein <johannes.t.klein@gmail.com>
2024-07-07 13:40:20 +0200

    AccountDeletion component for settings screen

---
+++ components/UIComponents/AccountDeletion.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Platform, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import i18n from "../../i18n";
+import { viewStyles } from "../../styles/uiComponents/cards/donateCard";
+import Button from "./Buttons/Button";
+import StyledText from "./StyledText";
+import { baseTextStyles } from "../../styles/textStyles";
+import { useAppOrientation } from "../Providers/AppOrientationProvider";
+
+const AccountDeletion = ( ) => {
+};
+
+export default AccountDeletion;

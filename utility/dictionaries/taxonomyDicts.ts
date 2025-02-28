d0029297b20e898ff51014b3dcb014184e721f53
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-28 23:17:09 +0100

    Add iconicTaxaNames

--- utility/dictionaries/taxonomyDicts.ts
+++ utility/dictionaries/taxonomyDicts.ts
@@ -1,3 +1,24 @@
+type IconicTaxaNames = {
+  [key: number]: string;
+};
+
+const iconicTaxaNames: IconicTaxaNames = {
+  47126: "species_detail.plant",
+  20978: "species_detail.amphibian",
+  47170: "species_detail.fungi",
+  47178: "species_detail.fish",
+  26036: "species_detail.reptile",
+  47119: "species_detail.arachnid",
+  3: "species_detail.bird",
+  47158: "species_detail.insect",
+  47115: "species_detail.mollusk",
+  40151: "species_detail.mammal",
+  1: "taxon_picker.other",
+  47686: "taxon_picker.other", // protozoans can be observed and have a non-animalia / iconic taxon group ancestor,
+  48222: "taxon_picker.other" // kelp
+};
+
+
 interface Ranks {
   [key: string]: string;
 }
@@ -172,6 +193,7 @@ const taxonDictForMissions: TaxaIds = {
 };
 
 export {
+  iconicTaxaNames,
   ranks,
   taxonDictForMissions,
   taxonIds

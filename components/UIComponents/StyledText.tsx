d9a9fecba13b8424ee6902837601d9887b983db9
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-24 21:03:04 +0200

    Remove superfluous props interface

--- components/UIComponents/StyledText.tsx
+++ components/UIComponents/StyledText.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, TextProps } from "react-native";
 
 import { leftText } from "../../styles/global";
 
-interface Props {
-  testID?: string;
-  children: React.ReactNode;
-  style?: object;
-  allowFontScaling?: boolean;
-  numberOfLines?: number;
-  onPress?: ( ) => void;
-}
-
-const StyledText = ( props: Props ) => {
+const StyledText = ( props: TextProps ) => {
   const { children } = props;
   return (
     <Text

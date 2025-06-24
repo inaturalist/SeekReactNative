82756f63408aa4a9227d88e46a38784859e10994
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-24 10:12:07 +0200

    Update text on login success screen

--- components/Auth/Login/LoginSuccessScreen.tsx
+++ components/Auth/Login/LoginSuccessScreen.tsx
@@ -10,6 +10,7 @@ import ScrollWithHeader from "../../UIComponents/Screens/ScrollWithHeader";
 import PrivacyAndTerms from "../../UIComponents/PrivacyAndTerms";
 import StyledText from "../../UIComponents/StyledText";
 import { baseTextStyles } from "../../../styles/textStyles";
+import BulletedList from "../../iNaturalist/BulletedList";
 
 const LoginSuccessScreen = ( ) => {
   const { navigate } = useNavigation();
@@ -24,17 +25,14 @@ const LoginSuccessScreen = ( ) => {
         <View style={styles.marginSmall} />
         <StyledText style={baseTextStyles.body}>{i18n.t( "inat_signup.posting_details" )}</StyledText>
         <View style={styles.marginMedium} />
-        <GreenText smaller text="inat_signup.observations" />
+        <GreenText smaller text="inat_signup.observations_not_synced" />
         <View style={styles.marginSmall} />
         <StyledText style={baseTextStyles.body}>
-          {i18n.t( "inat_signup.observations_1" )}
-          {" "}
-          <StyledText style={styles.underline}>
-            {i18n.t( "inat_signup.observations_2" )}
-          </StyledText>
-          {" "}
-          {i18n.t( "inat_signup.observations_3" )}
+          {i18n.t( "inat_signup.observations_stored_only_in_app" )}
         </StyledText>
+        <BulletedList text="inat_signup.observations_bullet_1" />
+        <BulletedList text="inat_signup.observations_bullet_2" />
+        <BulletedList text="inat_signup.observations_3" />
       </View>
       <View style={styles.marginLarge} />
       <GreenButton

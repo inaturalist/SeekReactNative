98e5823d68cc4edaf39db8a0793dd8770421e561
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-24 15:55:57 +0200

    Migrate TopSpacer to TS

--- components/UIComponents/TopSpacer.tsx
+++ components/UIComponents/TopSpacer.tsx
@@ -1,15 +1,13 @@
-// @flow
-
 import * as React from "react";
 import { View, Platform } from "react-native";
 
 import styles from "../../styles/uiComponents/topSpacer";
 
-type Props = {
-  backgroundColor?: ?string
-};
+interface Props {
+  readonly backgroundColor?: string | null;
+}
 
-const TopSpacer = ( { backgroundColor }: Props ): React.Node => {
+const TopSpacer = ( { backgroundColor }: Props ) => {
   if ( Platform.OS === "ios" ) {
     return (
       <View style={[styles.iosSpacer, backgroundColor && { backgroundColor }]} />

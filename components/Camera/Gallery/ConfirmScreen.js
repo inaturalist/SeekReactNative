f1d13b46306edd99a85eb176c5046682c265d696
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-28 22:25:01 +0200

    Remove unused effects

--- components/Camera/Gallery/ConfirmScreen.js
+++ components/Camera/Gallery/ConfirmScreen.js
@@ -1,29 +1,13 @@
 // @flow
 
-import React, { useEffect } from "react";
+import React from "react";
 import type { Node } from "react";
-import { useNavigation } from "@react-navigation/native";
 
 import ErrorScreen from "./Error";
 import { useObservation } from "../../Providers/ObservationProvider";
 
 const ConfirmScreen = ( ): Node => {
-  const navigation = useNavigation( );
-  const { observation, error, setError } = useObservation();
-
-  useEffect( ( ) => {
-    if ( observation && observation.taxon ) {
-      navigation.push( "Drawer", {
-        screen: "Match"
-      } );
-    }
-  }, [observation, navigation] );
-
-  useEffect( ( ) => {
-    navigation.addListener( "blur", ( ) => {
-      setError( null );
-    } );
-  }, [navigation, setError] );
+  const { error } = useObservation();
 
   if ( error ) {
     return (

7241cb21fd05ff35a0261a0f207a8b0eacf8a98d
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-28 22:13:46 +0200

    Remove simple components from ConfirmScreen

--- components/Camera/Gallery/ConfirmScreen.js
+++ components/Camera/Gallery/ConfirmScreen.js
@@ -1,17 +1,11 @@
 // @flow
 
 import React, { useEffect } from "react";
-import { View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import type { Node } from "react";
 import { useNavigation } from "@react-navigation/native";
 
-import { viewStyles, imageStyles } from "../../../styles/camera/confirm";
-import { colors } from "../../../styles/global";
-import LoadingWheel from "../../UIComponents/LoadingWheel";
 import GreenButton from "../../UIComponents/Buttons/GreenButton";
-import GreenText from "../../UIComponents/GreenText";
-import BackArrow from "../../UIComponents/Buttons/BackArrow";
 import ErrorScreen from "./Error";
 import { useObservation } from "../../Providers/ObservationProvider";
 
@@ -55,31 +49,9 @@ const ConfirmScreen = ( ): Node => {
 
   return (
     <SafeAreaView edges={["top"]}>
-      <View style={viewStyles.header}>
-        <BackArrow green />
-        <View style={viewStyles.headerText}>
-          <GreenText allowFontScaling={false} smaller text="confirm.identify" />
-        </View>
-        <View />
-      </View>
-      <View style={viewStyles.imageContainer}>
-        {clicked && (
-          <View style={viewStyles.loadingWheel}>
-            <LoadingWheel color={colors.white} />
-          </View>
-        )}
-        {image?.uri && <Image
-          source={{ uri: image.uri }}
-          style={imageStyles.image}
-        />}
-      </View>
-      <View style={viewStyles.footer}>
         <GreenButton
-          color={clicked && colors.seekTransparent}
           handlePress={updateClicked}
-          text="confirm.button"
         />
-      </View>
     </SafeAreaView>
   );
 };

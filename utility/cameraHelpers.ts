658cfa616e09c4b2b43f78f5ec6296ae74d2f617
Johannes Klein <johannes.t.klein@gmail.com>
2025-07-06 20:00:15 +0200

    Update cameraHelpers.ts

--- utility/cameraHelpers.ts
+++ utility/cameraHelpers.ts
@@ -1,17 +1,16 @@
-// @flow
 import { Platform, Alert } from "react-native";
 import { getSystemVersion } from "react-native-device-info";
 
 import { writeToDebugLog, checkPhotoSize } from "./photoHelpers";
 import i18n from "../i18n";
 
-const handleLog = ( event: { nativeEvent: { log: string } } ) => {
+const handleLog = ( event: { nativeEvent: { log: string } } ): void => {
   if ( Platform.OS === "android" ) {
     writeToDebugLog( event.nativeEvent.log );
   }
 };
 
-const showCameraSaveFailureAlert = async ( e: string, uri: string ) => {
+const showCameraSaveFailureAlert = async ( e: string, uri: string ): Promise<void> => {
   const size = await checkPhotoSize( uri );
   Alert.alert(
     i18n.t( "social.error_title" ),

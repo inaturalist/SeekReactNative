a0fd8dd3cfb15aebd5f2bed560aa077588a2ab0c
Johannes Klein <johannes.t.klein@gmail.com>
2025-07-21 15:35:48 +0200

    Update to react-native-reanimated latest (#769)
    * Update package.json
    
    * Update package-lock.json
    
    * Update Podfile.lock
    
    * Update jest.setup.js

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -17,6 +17,8 @@ import {
   mockUseCameraFormat
 } from "./vision-camera/vision-camera";
 
+require( "react-native-reanimated" ).setUpTests();
+
 jest.mock( "@react-native-async-storage/async-storage", () => mockAsyncStorage );
 jest.mock( "react-native-device-info", () => mockRNDeviceInfo );
 jest.mock( "react-native-localize", () => mockRNLocalize );
@@ -49,20 +51,6 @@ jest.mock( "react-native-exception-handler", () => ( {
     .fn()
     .mockImplementation( ( ) => mockErrorHandler() )
 } ) );
-
-// include this section and the NativeAnimatedHelper section for mocking react-native-reanimated
-jest.mock( "react-native-reanimated", () => {
-  const Reanimated = require( "react-native-reanimated/mock" );
-
-  // The mock for `call` immediately calls the callback which is incorrect
-  // So we override it with a no-op
-  Reanimated.default.call = () => {};
-
-  return Reanimated;
-} );
-
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-jest.mock( "react-native/Libraries/Animated/NativeAnimatedHelper" );
 
 jest.mock( "@react-navigation/native", () => {
   const actualNav = jest.requireActual( "@react-navigation/native" );

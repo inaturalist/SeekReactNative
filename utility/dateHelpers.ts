7ca317e1d935f5375c7c188be63d9f8955bfee56
Johannes Klein <johannes.t.klein@gmail.com>
2025-08-09 20:09:13 +0200

    Fix introduced crash

--- utility/dateHelpers.ts
+++ utility/dateHelpers.ts
@@ -15,8 +15,7 @@ import {
 import type {
   Locale
 } from "date-fns";
-import { toZonedTime, formatInTimeZone } from "date-fns-tz";
-
+import * as TimeZone from "date-fns-tz";
 import * as RNLocalize from "react-native-localize";
 import {
   af,

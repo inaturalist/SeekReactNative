4b563cf0d721b089806a857256306f8b9275f4cc
Johannes Klein <johannes.t.klein@gmail.com>
2026-02-26 01:03:58 +0100

    Add on press handler to user location icon

--- components/Home/SpeciesNearby/LocationMap.tsx
+++ components/Home/SpeciesNearby/LocationMap.tsx
@@ -27,36 +27,41 @@ const LocationMap = ( {
   onRegionChange,
   returnToUserLocation,
   posting = false,
-}: Props ) => (
-  <View style={viewStyles.container}>
-    {region.latitude ? (
-      <MapView
-        onRegionChangeComplete={onRegionChange}
-        provider={PROVIDER_DEFAULT}
-        region={region} // need region instead of initial region for return to user location
-        style={viewStyles.map}
-      />
-    ) : (
-      <View style={viewStyles.textContainer}>
-        <StyledText style={[baseTextStyles.body, textStyles.text]}>
-          {i18n.t( "species_nearby.input_location_above_map" )}
-        </StyledText>
+}: Props ) => {
+  const onUserLocationPressed = ( ) => {
+    returnToUserLocation( );
+  };
+  return (
+    <View style={viewStyles.container}>
+      {region.latitude ? (
+        <MapView
+          onRegionChangeComplete={onRegionChange}
+          provider={PROVIDER_DEFAULT}
+          region={region} // need region instead of initial region for return to user location
+          style={viewStyles.map}
+        />
+      ) : (
+        <View style={viewStyles.textContainer}>
+          <StyledText style={[baseTextStyles.body, textStyles.text]}>
+            {i18n.t( "species_nearby.input_location_above_map" )}
+          </StyledText>
+        </View>
+      )}
+      <View pointerEvents="none" style={viewStyles.pinFixed}>
+        {posting
+          ? <Image source={postingIcons.crosshair} />
+          : region.latitude ? <Image source={icons.locationPin} style={imageStyles.markerPin} /> : null}
       </View>
-    )}
-    <View pointerEvents="none" style={viewStyles.pinFixed}>
-      {posting
-        ? <Image source={postingIcons.crosshair} />
-        : region.latitude ? <Image source={icons.locationPin} style={imageStyles.markerPin} /> : null}
+      <TouchableOpacity
+        accessibilityLabel={i18n.t( "accessibility.user_location" )}
+        accessible
+        onPress={onUserLocationPressed}
+        style={viewStyles.locationIcon}
+      >
+        <Image source={icons.indicator} />
+      </TouchableOpacity>
     </View>
-    <TouchableOpacity
-      accessibilityLabel={i18n.t( "accessibility.user_location" )}
-      accessible
-      onPress={returnToUserLocation}
-      style={viewStyles.locationIcon}
-    >
-      <Image source={icons.indicator} />
-    </TouchableOpacity>
-  </View>
-);
+  )
+};
 
 export default LocationMap;

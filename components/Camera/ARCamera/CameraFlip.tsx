24edca71bf8066c1356d6c2811d8c0bcc116773c
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-08 18:36:25 +0200

    Add CameraFlip button
    Adapted from iNat's same component

---
+++ components/Camera/ARCamera/CameraFlip.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import i18n from "../../../i18n";
+import TransparentCircleButton from "../../UIComponents/Buttons/TransparentCircleButton";
+import icons from "../../../assets/icons";
+
+interface Props {
+  flipCamera: ( ) => void;
+}
+
+const CameraFlip = ( {
+  flipCamera,
+}: Props ) => {
+  return (
+    <TransparentCircleButton
+      onPress={flipCamera}
+      accessibilityLabel={i18n.t( "accessibility.flip_camera" )}
+      accessibilityHint={i18n.t( "accessibility.use_other_camera" )}
+      source={icons.iNat_valueprop_bullet_1}
+    />
+  );
+};
+
+export default CameraFlip;

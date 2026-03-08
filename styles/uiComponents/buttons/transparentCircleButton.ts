052689ceeda0b7b6fd1fcc6c03e7dd1c261ce6f4
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-08 20:35:43 +0200

    Add missing style for transparent button

---
+++ styles/uiComponents/buttons/transparentCircleButton.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const viewStyles = StyleSheet.create( {
+  wrapperStyle: {
+    backgroundColor: "#00000088",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 40,
+    height: 40,
+    borderRadius: 99,
+  },
+} );
+
+export default viewStyles;

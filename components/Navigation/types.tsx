ba509ab7610917391d7e365224b2d194b502b495
Johannes Klein <johannes.t.klein@gmail.com>
2026-06-02 12:31:24 +0200

    Update types.tsx

--- components/Navigation/types.tsx
+++ components/Navigation/types.tsx
@@ -9,6 +9,7 @@ import type { StackScreenProps } from "@react-navigation/stack";
 
 // Note from the documentation:
 // The type containing the mapping must be a type alias. It cannot be an interface.
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export type DrawerParamList = {
   Home: undefined;
   Achievements: undefined;
@@ -28,6 +29,7 @@ export type DrawerParamList = {
 
 // Note from the documentation:
 // The type containing the mapping must be a type alias. It cannot be an interface.
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export type RootStackParamList = {
   Splash: undefined;
   Onboarding: undefined;
@@ -67,7 +69,9 @@ export type DrawerStackScreenProps<T extends keyof DrawerParamList> =
 
 // https://reactnavigation.org/docs/typescript/?config=dynamic#specifying-default-types-for-usenavigation-link-ref-etc
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace ReactNavigation {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface RootParamList extends RootStackParamList {}
   }
 }

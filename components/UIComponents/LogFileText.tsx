14baf52bac0197cc557e2069c23d7970d01ef991
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-13 23:48:00 +0200

    Update LogFileText.tsx

--- components/UIComponents/LogFileText.tsx
+++ components/UIComponents/LogFileText.tsx
@@ -1,8 +1,7 @@
-import { readFile, stat } from "@dr.pogodin/react-native-fs";
-import React, { useEffect } from "react";
+import React from "react";
 import { ScrollView } from "react-native";
 
-import { pathLogs } from "../../utility/dirStorage";
+import { useLogPreview } from "../../utility/logManagementHelpers";
 import StyledText from "./StyledText";
 
 interface Props {
@@ -10,21 +9,15 @@ interface Props {
 }
 
 const LogFileText = ( {}: Props ) => {
-  const [logContents, setLogContents] = React.useState( "" );
+  const logPreview = useLogPreview();
 
-  const { mtime: logFileMtime } = stat( pathLogs );
-
-  useEffect( () => {
-    async function fetchLogContents() {
-      const contents = await readFile( pathLogs );
-      setLogContents( contents.split( "\n" ).slice( -100 ).join( "\n" ) );
-    }
-    fetchLogContents();
-  }, [logFileMtime] );
+  if ( !logPreview ) {
+    return null;
+  }
 
   return (
     <ScrollView>
-      <StyledText>{logContents}</StyledText>
+      <StyledText>{logPreview.text}</StyledText>
     </ScrollView>
   );
 };

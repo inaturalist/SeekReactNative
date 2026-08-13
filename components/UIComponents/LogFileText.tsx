a23149c8570fe1d738f2280ea9a59a42bd1f2c31
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-13 23:51:26 +0200

    Update LogFileText.tsx

--- components/UIComponents/LogFileText.tsx
+++ components/UIComponents/LogFileText.tsx
@@ -1,14 +1,11 @@
+import type { PropsWithChildren } from "react";
 import React from "react";
 import { ScrollView } from "react-native";
 
 import { useLogPreview } from "../../utility/logManagementHelpers";
 import StyledText from "./StyledText";
 
-interface Props {
-  navigation: any;
-}
-
-const LogFileText = ( {}: Props ) => {
+const LogFileText = ( {}: PropsWithChildren ) => {
   const logPreview = useLogPreview();
 
   if ( !logPreview ) {

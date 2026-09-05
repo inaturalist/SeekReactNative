b3d91b4d67fa0824649f7a68b7645dd25a1dc724
Johannes Klein <johannes.t.klein@gmail.com>
2026-09-05 10:53:44 +0200

    Port Android 36 back-gesture workaround (#1007)
    Co-authored-by: Johannes Klein <17345891+jtklein@users.noreply.github.com>

--- android/app/src/main/java/org/inaturalist/seek/MainActivity.kt
+++ android/app/src/main/java/org/inaturalist/seek/MainActivity.kt
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Build
 import android.os.Bundle
 
 class MainActivity : ReactActivity() {
@@ -27,4 +28,20 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /**
+   * https://github.com/facebook/react-native/issues/54887
+   * workaround from that thread for a bug in Android 36+ & RN < 0.84 where
+   * the app would get in a state where back gesture/press from anywhere in the
+   * app would dismiss. This skips the buggy React behavior and is gated to the
+   * API version that introduced it.
+   * TODO: remove and validate upstream fix after 0.84 upgrade (MOB-1740)
+   */
+  override fun invokeDefaultOnBackPressed() {
+    if (Build.VERSION.SDK_INT >= 36) {
+      moveTaskToBack(true)
+    } else {
+      super.invokeDefaultOnBackPressed()
+    }
+  }
 }

43a40bb3bb3c495c12038ab6dc978c67b367e555
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-09-05 13:58:44 +0200

    Add central switcher for staging oauth api url

--- utility/envConfig.ts
+++ utility/envConfig.ts
@@ -1,5 +1,17 @@
 import config from "../config";
+
+const PRODUCTION = {
+  writeApiURL: "https://www.inaturalist.org",
+};
+
+const STAGING = {
+  writeApiURL: "https://staging.inaturalist.org",
+};
+
+const urls = __DEV__ ? STAGING : PRODUCTION;
+
 const EnvConfig = {
+  oauthApiURL: urls.writeApiURL,
   jwtSecret: __DEV__
     ? config.stagingJwtSecret || config.jwtSecret
     : config.jwtSecret,
